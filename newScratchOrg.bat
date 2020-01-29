4276df909c37871cfd275c52d651e6d7cbbc59a9
Carl Huseby Fosli <carl.fosli@gmail.com>
2020-01-29 10:33:00 +0100

    Update newScratchOrg.bat
    Lagt til ny versjon av pakken crm-platform-access-control: 0.5.0.4

--- newScratchOrg.bat
+++ newScratchOrg.bat
@@ -1,4 +1,4 @@
 call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --durationdays 2 --setdefaultusername --json --loglevel fatal  --wait 10
 call sfdx force:package:install --package 04t2o000001MxPAAA0  -k navcrm --wait 10 --publishwait 10
-call sfdx force:package:install --package 04t2o000001MxPyAAK  -k navcrm --wait 10 --publishwait 10
+call sfdx force:package:install --package 04t2o000001MxQhAAK  -k navcrm --wait 10 --publishwait 10
 call sfdx force:source:push
