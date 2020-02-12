d81fc5b4fcc5ac684a6074d5062f4329ca2f725d
vmjelde <vegard.mjelde@soprasteria.com>
2020-02-12 13:19:22 +0100

    Added permset:assign (Assigns HOT_admin to systAdmin)

--- newScratchOrg.bat
+++ newScratchOrg.bat
@@ -2,3 +2,4 @@ call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --du
 call sfdx force:package:install --package 04t2o000001MxPAAA0  -k navcrm --wait 10 --publishwait 10
 call sfdx force:package:install --package 04t2o000001MxQhAAK  -k navcrm --wait 10 --publishwait 10
 call sfdx force:source:push
+call sfdx force:user:permset:assign --permsetname HOT_admin
