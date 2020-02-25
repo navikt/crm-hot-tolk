bea6fcd3377843fa63f21fffaeb7b9792fdce918
Carl Huseby Fosli <carl.fosli@gmail.com>
2020-02-25 19:53:21 +0100

    Lagt til konfigurasjon fra hottest
    https://jira.adeo.no/browse/TOLK-54

--- newScratchOrg.bat
+++ newScratchOrg.bat
@@ -2,6 +2,7 @@ call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --du
 call sfdx force:package:install --package 04t2o000001MxZjAAK  -k navcrm --wait 10 --publishwait 10
 call sfdx force:package:install --package 04t2o000001MxYWAA0  -k navcrm --wait 10 --publishwait 10
 ::call sfdx force:source:deploy -p C:\dev\workspace\crm-platform-unpackaged\force-app\main\default\objects\PersonAccount
+::call sfdx force:source:retrieve -u carl.fosli@nav.no.hottest -x fsl-metadata\package.xml
 call sfdx force:source:push
 call sfdx force:user:permset:assign --permsetname HOT_admin
 if %2==imp call sfdx force:data:tree:import --plan data-source/data-import-plan.json
