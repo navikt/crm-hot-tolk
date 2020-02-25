fe6b6b1d5b2ab2877b9c11fe2fff25998b4da260
Carl Huseby Fosli <carl.fosli@gmail.com>
2020-02-25 21:05:06 +0100

    Update newScratchOrg.bat

--- newScratchOrg.bat
+++ newScratchOrg.bat
@@ -1,5 +1,7 @@
 call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --durationdays 2 --setdefaultusername --json --loglevel fatal  --wait 10
+:: Install crm-platform-base 
 call sfdx force:package:install --package 04t2o000001MxZjAAK  -k navcrm --wait 10 --publishwait 10
+:: Install crm-platform-access-control
 call sfdx force:package:install --package 04t2o000001MxYWAA0  -k navcrm --wait 10 --publishwait 10
 ::call sfdx force:source:deploy -p C:\dev\workspace\crm-platform-unpackaged\force-app\main\default\objects\PersonAccount
 ::call sfdx force:source:retrieve -u carl.fosli@nav.no.hottest -x fsl-metadata\package.xml
