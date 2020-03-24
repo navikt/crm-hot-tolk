1cb5980ab7fc2e3c553338163e1456240cb06e31
vmjelde <vegard.mjelde@soprasteria.com>
2020-03-24 14:25:40 +0100

    Welcome message to user at home page
    https://jira.adeo.no/browse/TOLK-121

--- force-app/main/default/lwc/hot_home/hot_home.js
+++ force-app/main/default/lwc/hot_home/hot_home.js
@@ -1,7 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
+import { getRecord } from 'lightning/uiRecordApi';
+import USER_ID from '@salesforce/user/Id';
+import NAME_FIELD from '@salesforce/schema/User.FirstName';
 
 export default class Hot_home extends NavigationMixin(LightningElement) {
+
+	@track name;
+	@track error;
+	@wire(getRecord, {
+		recordId: USER_ID,
+		fields: [NAME_FIELD]
+	}) wireuser({
+		error,
+		data
+	}) {
+		if (error) {
+			this.error = error;
+		} else if (data) {
+			this.name = data.fields.FirstName.value;
+
+		}
+	}
 
 	goToMyRequests() {
 		this[NavigationMixin.Navigate]({
