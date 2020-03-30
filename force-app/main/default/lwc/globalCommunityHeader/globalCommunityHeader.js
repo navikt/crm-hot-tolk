30f6a1a8d651fb1752c06f6957cff1a01b2276cb
vmjelde <vegard.mjelde@soprasteria.com>
2020-03-30 15:03:54 +0200

    Added hamburger-menu-ikon
    https://jira.adeo.no/browse/TOLK-164

--- force-app/main/default/lwc/globalCommunityHeader/globalCommunityHeader.js
+++ force-app/main/default/lwc/globalCommunityHeader/globalCommunityHeader.js
@@ -5,6 +5,7 @@ import CURRENT_USER from '@salesforce/schema/User.Name';
 import IDPORTEN_IKON from '@salesforce/resourceUrl/idporten_ikon';
 import LOGO from '@salesforce/resourceUrl/logo';
 import SEARCH_BUTTON from '@salesforce/resourceUrl/sok_liten_morkeblaa';
+import HAMBURGER_MENY from '@salesforce/resourceUrl/hamburger_meny_ikon';
 
 const fields = [CURRENT_USER];
 
@@ -20,6 +21,7 @@ export default class GlobalHeader extends LightningElement {
 	logo = LOGO;
 	idportenIkon = IDPORTEN_IKON;
 	searchButton = SEARCH_BUTTON;
+	hamburgerMenyIkon = HAMBURGER_MENY;
 
 	toggleMobileMenu() {
 		var x = this.template.querySelector(".accessible-megamenu");
