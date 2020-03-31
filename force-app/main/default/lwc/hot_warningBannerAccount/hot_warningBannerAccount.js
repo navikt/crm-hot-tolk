3d9c08cf0b16a6b3499c6af968b2cb2863f5dbf8
Carl Huseby Fosli <carl.fosli@gmail.com>
2020-03-31 15:20:43 +0200

    Lagt til støtte for reservasjoner i banner
    https://jira.adeo.no/browse/TOLK-167

--- force-app/main/default/lwc/hot_warningBannerAccount/hot_warningBannerAccount.js
+++ force-app/main/default/lwc/hot_warningBannerAccount/hot_warningBannerAccount.js
@@ -1,11 +1,12 @@
 import { LightningElement, wire, api } from "lwc";
 import { getRecord, getFieldValue } from "lightning/uiRecordApi";
 import securityMeasures from '@salesforce/schema/Account.CRM_Person__r.INT_SecurityMeasures__c';
+import reservations from '@salesforce/schema/Account.CRM_Person__r.HOT_Reservations__c';
 
-export default class Hot_securityMeasuresWarning extends LightningElement {
+export default class Hot_warningBannerAccount extends LightningElement {
 	@api recordId;
 
-	@wire(getRecord, { recordId: "$recordId", fields: [securityMeasures] })
+	@wire(getRecord, { recordId: "$recordId", fields: [securityMeasures, reservations] })
 	record;
 
 	get securityMeasures() {
@@ -14,5 +15,13 @@ export default class Hot_securityMeasuresWarning extends LightningElement {
 
 	get hasSecurityMeasures() {
 		return getFieldValue(this.record.data, securityMeasures) != null;
+	}
+
+	get reservations() {
+		return getFieldValue(this.record.data, reservations);
+	}
+
+	get hasReservations() {
+		return getFieldValue(this.record.data, reservations) != null;
 	}
 }
