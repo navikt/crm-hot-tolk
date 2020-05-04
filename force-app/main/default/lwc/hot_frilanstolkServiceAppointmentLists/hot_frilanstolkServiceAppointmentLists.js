10092198993524522eaa81ee4b242439677f9359
vmjelde <60097379+vmjelde@users.noreply.github.com>
2020-05-04 08:27:22 +0200

    Update hot_frilanstolkServiceAppointmentLists.js

--- force-app/main/default/lwc/hot_frilanstolkServiceAppointmentLists/hot_frilanstolkServiceAppointmentLists.js
+++ force-app/main/default/lwc/hot_frilanstolkServiceAppointmentLists/hot_frilanstolkServiceAppointmentLists.js
@@ -1,7 +1,7 @@
 import { LightningElement, wire, track, api } from 'lwc';
 import getAllServiceAppointments from '@salesforce/apex/HOT_ServiceAppointmentListControllerAll.getAllServiceAppointments';
 import getMyServiceAppointments from '@salesforce/apex/HOT_ServiceAppointmentListControllerMy.getMyServiceAppointments';
-import createServiceConnection from '@salesforce/apex/HOT_CreateServiceConnection.createServiceConnection';
+import createServiceConnection from '@salesforce/apex/HOT_CreateInterestedResource.createInterestedResource';
 import { createRecord } from 'lightning/uiRecordApi';
 import { refreshApex } from '@salesforce/apex';
 import { NavigationMixin } from 'lightning/navigation';
