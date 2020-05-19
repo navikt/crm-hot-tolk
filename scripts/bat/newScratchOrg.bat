20c85006340714a040eef15e8a75c60a2191762e
Carl Huseby Fosli <carl.fosli@gmail.com>
2020-05-19 20:20:57 +0200

    Update newScratchOrg.bat

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -1,5 +1,5 @@
 :: Opprett en scratch org
-call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --durationdays 2 --setdefaultusername --json --loglevel fatal  --wait 10
+call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --durationdays %2 --setdefaultusername --json --loglevel fatal  --wait 10
 
 :: Installer crm-platform-base 
 call sfdx force:package:install --package 04t2o000001My9SAAS  -k navcrm --wait 10 --publishwait 10
