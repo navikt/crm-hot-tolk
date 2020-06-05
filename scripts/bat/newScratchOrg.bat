e774a181ae97bf930c79ef89f6b909bbbba11e69
Carl Huseby Fosli <carl.fosli@gmail.com>
2020-06-05 19:45:26 +0200

    Oppdatert dependencies

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -2,13 +2,13 @@
 call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --durationdays %2 --setdefaultusername --json --loglevel fatal  --wait 10
 
 :: Installer crm-platform-base 
-call sfdx force:package:install --package 04t2o000000OA1dAAG  -k navcrm --wait 10 --publishwait 10
+call sfdx force:package:install --package 04t2o000000OA5aAAG  -k navcrm --wait 10 --publishwait 10
 
 :: Installer crm-platform-access-control
 call sfdx force:package:install --package 04t2o000000OA22AAG  -k navcrm --wait 10 --publishwait 10
 
 :: Installer crm-community-base
-call sfdx force:package:install --package 04t2o000000OA4hAAG  -k navcrm --wait 10 --publishwait 10
+call sfdx force:package:install --package 04t2o000000OA9SAAW  -k navcrm --wait 10 --publishwait 10
 
 :: Dytt kildekoden til scratch org'en
 call sfdx force:source:push
