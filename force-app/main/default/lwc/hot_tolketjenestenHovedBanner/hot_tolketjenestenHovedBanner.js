c29c1c8dbab0b18d0d15087e08f856231e1de316
vmjelde <vegard.mjelde@soprasteria.com>
2020-06-08 14:04:42 +0200

    Update hot_tolketjenestenHovedBanner.js

--- force-app/main/default/lwc/hot_tolketjenestenHovedBanner/hot_tolketjenestenHovedBanner.js
+++ force-app/main/default/lwc/hot_tolketjenestenHovedBanner/hot_tolketjenestenHovedBanner.js
@@ -2,7 +2,6 @@ import { LightningElement, track } from 'lwc';
 
 
 export default class Hot_tolketjenestenHovedBanner extends LightningElement {
-	@track isProd = window.location.toString().includes("tolkebestilling.nav.no/");
 
 
 }
