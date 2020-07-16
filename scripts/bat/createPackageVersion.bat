6d2aa2372d8d4bb524faa227a1fd9edf5779880a
vmjelde <vegard.mjelde@soprasteria.com>
2020-07-16 19:14:32 +0200

    Update createPackageVersion.bat

--- scripts/bat/createPackageVersion.bat
+++ scripts/bat/createPackageVersion.bat
@@ -2,7 +2,7 @@
 call move force-app\unpackagable temp
 
 :: Opprett en pakkeversjon
-call sfdx force:package:version:create -f config/project-scratch-def.json -k navcrm -p crm-hot-dev --wait 60
+call sfdx force:package:version:create -f config/project-scratch-def.json -k navcrm -p crm-hot --wait 60
 :: for ($i=1; $i -le 100; $i++) {sfdx force:package:version:create:report -i XXX}
 
 :: Flytt mapper og filer tilbake
