cc64800340c0371c86806941eaeadf13845b3cbf
vmjelde <vegard.mjelde@soprasteria.com>
2020-07-20 13:12:17 +0200

    Fixed code
    https://jira.adeo.no/browse/TOLK-597

--- force-app/main/default/lwc/hot_userContactInformation/hot_userContactInformation.js
+++ force-app/main/default/lwc/hot_userContactInformation/hot_userContactInformation.js
@@ -1,6 +1,14 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getPersonPhoneEmail from '@salesforce/apex/HOT_UserInformationController.getPersonPhoneEmail';
 
 export default class hot_userContactInformation extends LightningElement {
-	@wire(getPersonPhoneEmail) person;
+	@track person;
+	@track recordId;
+	@wire(getPersonPhoneEmail)
+	wiredPerson(result) {
+		if (result.data) {
+			this.person = result.data;
+			this.recordId = this.person.Id;
+		}
+	}
 }
