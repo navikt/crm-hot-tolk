1b4f3f875728ae2e16148e0534c6be735d056b93
vmjelde <vegard.mjelde@soprasteria.com>
2020-08-26 15:19:24 +0200

    Endret flyt til forlaget til Lena
    https://jira.adeo.no/browse/TOLK-658

--- force-app/main/default/lwc/hot_home/hot_home.js
+++ force-app/main/default/lwc/hot_home/hot_home.js
@@ -5,7 +5,6 @@ import USER_ID from '@salesforce/user/Id';
 import NAME_FIELD from '@salesforce/schema/User.FirstName';
 import checkAssignedPermissionSet from '@salesforce/apex/HOT_Utility.checkAssignedPermissionSet'
 import checkAssignedPermissionSetGroup from '@salesforce/apex/HOT_Utility.checkAssignedPermissionSetGroup'
-import checkUserHasEntitlement from '@salesforce/apex/HOT_Utility.checkUserHasEntitlement';
 import isProdFunction from '@salesforce/apex/GlobalCommunityHeaderFooterController.isProd';
 
 export default class Hot_home extends NavigationMixin(LightningElement) {
@@ -58,55 +57,11 @@ export default class Hot_home extends NavigationMixin(LightningElement) {
 			});
 		}
 	}
-	goToRequestOnBehalf(event) {
-		if (!this.isProd) {
-			event.preventDefault();
-			this[NavigationMixin.Navigate]({
-				type: 'comm__namedPage',
-				attributes: {
-					pageName: 'ny-bestilling'
-				},
-				state: {
-					notDefault: true,
-				}
-			});
-		}
-	}
 
 	@track isFrilans = false;
-	@track isTolkUser = true;
-	@track isUser = false;
-
-	@track showFrilans = false;
-	@track showTolkUser = false;
-	@track showUser = false;
 	@wire(checkAssignedPermissionSetGroup, { permissionSetGroupName: 'HOT_Tolk_Frilans_Gruppe' })
 	async wireIsFrilans({ error, data }) {
 		this.isFrilans = data;
-		this.isTolkUser = await checkUserHasEntitlement();
-		this.isUser = !this.isTolkUser && !this.isFrilans;
-		if (this.isFrilans) {
-			this.showFrilans = true;
-			this.showTolkUser = false;
-			this.showUser = false;
-		}
-		else if (this.isTolkUser == true) {
-			this.showFrilans = false;
-			this.showTolkUser = true;
-			this.showUser = false;
-		}
-		else {
-			this.showFrilans = false;
-			this.showTolkUser = false;
-			this.showUser = true;
-		}
-
-		console.log("isFrilans: " + this.isFrilans);
-		console.log("isTolkUser: " + this.isTolkUser);
-		console.log("isUser: " + this.isUser);
-		console.log("showFrilans: " + this.showFrilans);
-		console.log("showTolkUser: " + this.showTolkUser);
-		console.log("showUser: " + this.showUser);
 
 
 	}
@@ -114,42 +69,8 @@ export default class Hot_home extends NavigationMixin(LightningElement) {
 	wireIsAdmin({ error, data }) {
 		if (!this.isFrilans) {
 			this.isFrilans = data;
-			this.isTolkUser = true; //checkUserHasEntitlement();
-			this.isUser = !this.isTolkUser && !this.isFrilans;
-			if (this.isFrilans) {
-				this.showFrilans = true;
-			}
-			else if (this.isTolkUser) {
-				this.showTolkUser = true;
-			}
-			else {
-				this.showUser = true;
-			}
 		}
 	}
-
-
-	@track showChoices = false;
-	handleShowChoices() {
-		this.showChoices = !this.showChoices;
-	}
-
-	changeToFreelance() {
-		this.showFrilans = true;
-		this.showTolkUser = false;
-		this.showUser = false;
-	}
-	changeToTolkUser() {
-		this.showFrilans = false;
-		this.showTolkUser = true;
-		this.showUser = false;
-	}
-	changeToUser() {
-		this.showFrilans = false;
-		this.showTolkUser = false;
-		this.showUser = true;
-	}
-
 
 
 	goToHome(event) {
