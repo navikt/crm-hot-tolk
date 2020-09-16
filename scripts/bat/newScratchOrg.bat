976ec2741d9d14e792fba28f4c849646999798b8
vmjelde <vegard.mjelde@soprasteria.com>
2020-09-16 20:49:35 +0200

    Update newScratchOrg.bat
    https://jira.adeo.no/browse/TOLK-734

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -17,5 +17,5 @@ call sfdx force:source:push
 call sfdx force:user:permset:assign --permsetname HOT_admin
 
 :: Opprett testdata
-call sfdx force:data:tree:import --plan data-source/data-import-plan.json
+:: call sfdx force:data:tree:import --plan data-source/data-import-plan.json
 call sfdx force:apex:execute -f scripts/apex/createTestData.apex
