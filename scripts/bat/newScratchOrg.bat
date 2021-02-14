b118aa30a01116135d462e4deae9678995103c3e
Carl Huseby Fosli <carl@fosli.net>
2021-02-14 21:35:45 +0100

    Flyttet KRR til crm-platform-integration

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -1,8 +1,11 @@
 :: Opprett en scratch org
 call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --durationdays %2 --setdefaultusername --json --loglevel fatal  --wait 10
 
-:: Installer crm-platform-base ver. 0.51
-call sfdx force:package:install --package 04t2o000000yQhkAAE -r -k navcrm --wait 10 --publishwait 10
+:: Installer crm-platform-base ver. 0.61
+call sfdx force:package:install --package 04t2o000000yR8bAAE -r -k navcrm --wait 10 --publishwait 10
+
+:: Installer crm-platform-integration ver. 0.33
+call sfdx force:package:install --package 04t2o000000yRI7AAM -r -k navcrm --wait 10 --publishwait 10
 
 :: Installer crm-platform-access-control ver. 0.50
 call sfdx force:package:install --package 04t2o000000yQtHAAU -r -k navcrm --wait 10 --publishwait 10
