ac00d6ff076c45f71d6ada61c88eddcf95f8be7e
Carl Huseby Fosli <carl@fosli.net>
2021-02-15 15:21:30 +0100

    Oppdatert avhengighet til crm-platform-integration

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -4,8 +4,8 @@ call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --du
 :: Installer crm-platform-base ver. 0.61
 call sfdx force:package:install --package 04t2o000000yR8bAAE -r -k navcrm --wait 10 --publishwait 10
 
-:: Installer crm-platform-integration ver. 0.33
-call sfdx force:package:install --package 04t2o000000yRI7AAM -r -k navcrm --wait 10 --publishwait 10
+:: Installer crm-platform-integration ver. 0.34
+call sfdx force:package:install --package 04t2o000000yRIgAAM -r -k navcrm --wait 10 --publishwait 10
 
 :: Installer crm-platform-access-control ver. 0.50
 call sfdx force:package:install --package 04t2o000000yQtHAAU -r -k navcrm --wait 10 --publishwait 10
