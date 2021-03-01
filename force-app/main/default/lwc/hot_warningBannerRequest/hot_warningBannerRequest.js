1987af54b45bbddc3f05ee8388825bd8624ab4c3
vmjelde <vegard.mjelde@soprasteria.com>
2021-03-01 13:02:17 +0100

    Update hot_warningBannerRequest.js
    https://jira.adeo.no/browse/TOLK-963

--- force-app/main/default/lwc/hot_warningBannerRequest/hot_warningBannerRequest.js
+++ force-app/main/default/lwc/hot_warningBannerRequest/hot_warningBannerRequest.js
@@ -2,19 +2,26 @@ import { LightningElement, wire, api, track } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import securityMeasures from '@salesforce/schema/HOT_Request__c.Account__r.CRM_Person__r.INT_SecurityMeasures__c';
 import reservations from '@salesforce/schema/HOT_Request__c.Account__r.CRM_Person__r.HOT_Reservations__c';
-import START_TIME from '@salesforce/schema/HOT_Request__c.StartTime__c';
-import END_TIME from '@salesforce/schema/HOT_Request__c.EndTime__c';
-import ACCOUNT from '@salesforce/schema/HOT_Request__c.Account__c';
-import getRequestListFromAccountFromRequestId from '@salesforce/apex/HOT_RequestListContoller.getRequestListFromAccountFromRequestId';
+import ACCOUNT_ID from '@salesforce/schema/HOT_Request__c.Account__c';
+import getOverlappingRecordsFromRequestId from '@salesforce/apex/HOT_DuplicateHandler.getOverlappingRecordsFromRequestId';
 
 export default class Hot_warningBannerRequest extends LightningElement {
     @api recordId;
+    @track record;
 
     @wire(getRecord, {
         recordId: '$recordId',
-        fields: [securityMeasures, reservations, START_TIME, END_TIME, ACCOUNT]
+        fields: [securityMeasures, reservations, ACCOUNT_ID]
     })
-    record;
+    wiredGetRecord(result) {
+        console.log('wiredGetRecord');
+        this.record = result;
+        if (result.data) {
+            console.log('true');
+            this.getDuplicates();
+        }
+        console.log('done');
+    }
 
     get securityMeasures() {
         return getFieldValue(this.record.data, securityMeasures).replace(/;/g, ', ');
@@ -35,66 +42,19 @@ export default class Hot_warningBannerRequest extends LightningElement {
         return getFieldValue(this.record.data, reservations) != null;
     }
 
-    @track allRequests;
-    @track requests;
-    @track error;
-    wiredRequestsResult;
-    @api accountId;
-
-    @wire(getRequestListFromAccountFromRequestId, { accountId: '$recordId' })
-    wiredRequest(result) {
-        this.wiredRequestsResult = result;
-        if (result.data) {
-            this.allRequests = result.data;
-            this.filterRequests();
-            this.error = undefined;
-        } else if (result.error) {
-            this.error = result.error;
-            this.allRequests = undefined;
-        }
-    }
-    isActive = false;
-    filterRequests() {
-        var tempRequests = [];
-        for (var i = 0; i < this.allRequests.length; i++) {
-            if (
-                this.allRequests[i].ExternalRequestStatus__c != 'Avlyst' &&
-                this.allRequests[i].ExternalRequestStatus__c != 'Dekket' &&
-                this.allRequests[i].ExternalRequestStatus__c != 'Udekket'
-            ) {
-                tempRequests.push(this.allRequests[i]);
-                if (this.allRequests[i].Id == this.recordId) {
-                    this.isActive = true;
-                }
-            }
-        }
-        this.requests = tempRequests;
-    }
-
     @track duplicateRequests = [];
-    isDuplicate() {
-        if (this.requests && this.isActive) {
-            for (var i = 0; i < this.requests.length; i++) {
-                if (
-                    ((this.requests[i].StartTime__c < getFieldValue(this.record.data, START_TIME) &&
-                        getFieldValue(this.record.data, START_TIME) < this.requests[i].EndTime__c) ||
-                        (getFieldValue(this.record.data, START_TIME) < this.requests[i].StartTime__c &&
-                            this.requests[i].StartTime__c < getFieldValue(this.record.data, END_TIME)) ||
-                        (getFieldValue(this.record.data, START_TIME) == this.requests[i].StartTime__c &&
-                            this.requests[i].EndTime__c == getFieldValue(this.record.data, END_TIME))) &&
-                    this.requests[i].Id != this.recordId
-                ) {
-                    var arr = {
-                        Id: '/' + this.requests[i].Id,
-                        Name: this.requests[i].Name
-                    };
-                    this.duplicateRequests.push(arr);
-                }
-            }
-        }
-    }
-    get getHasDuplicates() {
-        this.isDuplicate();
-        return this.duplicateRequests.length > 0 && this.isActive;
+    @track hasDuplicates = false;
+    async getDuplicates() {
+        console.log('getFieldValue(this.record.data, ACCOUNT_ID): ' + getFieldValue(this.record.data, ACCOUNT_ID));
+        console.log('this.recordId: ' + this.recordId);
+        let requestId = this.recordId;
+        let accountId = getFieldValue(this.record.data, ACCOUNT_ID);
+        let result = await getOverlappingRecordsFromRequestId({ accountId, requestId });
+        console.log(result);
+        console.log(JSON.stringify(result));
+        this.duplicateRequests = result;
+        this.hasDuplicates = this.duplicateRequests.length > 0;
+        console.log(JSON.stringify(this.duplicateRequests));
+        console.log(this.hasDuplicates);
     }
 }
