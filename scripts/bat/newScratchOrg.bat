6b41da7e3898a8ac8b9a229e0e04a3165c981df3
Carl Huseby Fosli <carl@fosli.net>
2021-06-02 14:55:16 +0200

    Oppdatert til crm-platform-base v. 0.93

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -1,8 +1,8 @@
 echo "Oppretter scratch org"
 call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --durationdays %2 --setdefaultusername --json --loglevel fatal  --wait 10
 
-echo "Installerer crm-platform-base ver. 0.86"
-call sfdx force:package:install --package 04t2o000000yRw8AAE -r -k navcrm --wait 10 --publishwait 10
+echo "Installerer crm-platform-base ver. 0.93"
+call sfdx force:package:install --package 04t2o000000yS5ZAAU -r -k navcrm --wait 10 --publishwait 10
 
 echo "Installerer crm-platform-integration ver. 0.44"
 call sfdx force:package:install --package 04t2o000000yS5FAAU -r -k navcrm --wait 10 --publishwait 10
