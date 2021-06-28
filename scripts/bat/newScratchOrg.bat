470c85293b916b049476aed06cc1393f5fbe9ac4
eirikfladby <eirik.fladby@nav.no>
2021-06-28 10:34:35 +0200

    updated package versions

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -4,7 +4,7 @@ call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --du
 echo "Installerer crm-platform-base ver. 0.97"
 call sfdx force:package:install --package 04t2o000000ySFFAA2 -r -k navcrm --wait 10 --publishwait 10
 
-echo "Installerer crm-platform-integration ver. 0.44"
+echo "Installerer crm-platform-integration ver. 0.50"
 call sfdx force:package:install --package 04t2o000000yS5FAAU -r -k navcrm --wait 10 --publishwait 10
 
 echo "Installerer crm-platform-access-control ver. 0.68"
