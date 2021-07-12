19c1ed1b6299ee62911dac6da4659ee2cb3e4ddf
Lasse <lasse.wulff@nav.no>
2021-07-12 15:30:57 +0200

    Resolved pull request review

--- force-app/main/freelanceCommunity/lwc/hot_frilanstolkUserInformation/hot_frilanstolkUserInformation.js
+++ force-app/main/freelanceCommunity/lwc/hot_frilanstolkUserInformation/hot_frilanstolkUserInformation.js
@@ -2,18 +2,13 @@ import { LightningElement, wire, track } from 'lwc';
 import getServiceResource from '@salesforce/apex/HOT_FreelanceUserInformationController.getServiceResource';
 
 export default class Hot_frilanstolkUserInformation extends LightningElement {
-    @track serviceResource;
-    @track name;
+    @track ServiceResource;
     @track recordId;
     @wire(getServiceResource)
     wiredGetServiceResource(result) {
         if (result.data) {
-            this.serviceResource = result.data;
-            this.recordId = this.serviceResource.Id;
-            this.name = this.serviceResource.Name;
-            this.HOT_AddressOverride__c = this.serviceResource.HOT_AddressOverride__c;
-            this.HOT_Address__c = this.serviceResource.HOT_Address__c;
-            this.HOT_IsAvailableForAcuteAssignments__c = this.serviceResource.HOT_IsAvailableForAcuteAssignments__c;
+            this.ServiceResource = result.data;
+            this.recordId = this.ServiceResource.Id;
         }
     }
 }
