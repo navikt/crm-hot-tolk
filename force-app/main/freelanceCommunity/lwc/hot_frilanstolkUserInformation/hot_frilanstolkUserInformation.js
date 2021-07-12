78f9f3f1ff0b5e2a802a56cc390f643daa974ff4
Vemund Eggemoen <vemund.eggemoen@hotmail.com>
2021-07-12 14:14:10 +0200

    WIP: fix pull request request review

--- force-app/main/freelanceCommunity/lwc/hot_frilanstolkUserInformation/hot_frilanstolkUserInformation.js
+++ force-app/main/freelanceCommunity/lwc/hot_frilanstolkUserInformation/hot_frilanstolkUserInformation.js
@@ -1,31 +1,19 @@
 import { LightningElement, wire, track } from 'lwc';
-//import getPerson from '@salesforce/apex/HOT_UserInformationController.getPerson';
 import getServiceResource from '@salesforce/apex/HOT_FreelanceUserInformationController.getServiceResource';
 
-
-
 export default class Hot_frilanstolkUserInformation extends LightningElement {
-    @track ServiceResource;
-    @track Name;
+    @track serviceResource;
+    @track name;
     @track recordId;
-    // @track HOT_AddressOverride__c;
-    // @track HOT_Address__c;
-    // @track HOT_IsAvailableForAcuteAssignments__c;
-
     @wire(getServiceResource)
     wiredGetServiceResource(result) {
         if (result.data) {
-            this.ServiceResource = result.data;
-            this.recordId = this.ServiceResource.Id;
-            this.Name = this.ServiceResource.Name;
-            this.HOT_AddressOverride__c =this.ServiceResource.HOT_AddressOverride__c;
-            this.HOT_Address__c = this.ServiceResource.HOT_Address__c;
-            this.HOT_IsAvailableForAcuteAssignments__c = this.ServiceResource.HOT_IsAvailableForAcuteAssignments__c;
-           
-            //this.RelatedRecordId = this.person.Id;
+            this.serviceResource = result.data;
+            this.recordId = this.serviceResource.Id;
+            this.name = this.serviceResource.Name;
+            this.HOT_AddressOverride__c = this.serviceResource.HOT_AddressOverride__c;
+            this.HOT_Address__c = this.serviceResource.HOT_Address__c;
+            this.HOT_IsAvailableForAcuteAssignments__c = this.serviceResource.HOT_IsAvailableForAcuteAssignments__c;
         }
-        console.log(getServiceResource() + "get service resource kall");
-        console.log(JSON.stringify(this.ServiceResource) + " this.serviceresource");
-       console.log(JSON.stringify(this.Name));
     }
 }
