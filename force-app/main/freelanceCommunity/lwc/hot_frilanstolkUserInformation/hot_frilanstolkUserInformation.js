177268975e5decb0af2c2d269fceacc482628afc
lassew001 <86602322+lassew001@users.noreply.github.com>
2021-07-13 08:38:24 +0200

    Update hot_frilanstolkUserInformation.js
    fixed

--- force-app/main/freelanceCommunity/lwc/hot_frilanstolkUserInformation/hot_frilanstolkUserInformation.js
+++ force-app/main/freelanceCommunity/lwc/hot_frilanstolkUserInformation/hot_frilanstolkUserInformation.js
@@ -2,13 +2,13 @@ import { LightningElement, wire, track } from 'lwc';
 import getServiceResource from '@salesforce/apex/HOT_FreelanceUserInformationController.getServiceResource';
 
 export default class Hot_frilanstolkUserInformation extends LightningElement {
-    @track ServiceResource;
+    @track serviceResource;
     @track recordId;
     @wire(getServiceResource)
     wiredGetServiceResource(result) {
         if (result.data) {
-            this.ServiceResource = result.data;
-            this.recordId = this.ServiceResource.Id;
+            this.serviceResource = result.data;
+            this.recordId = this.serviceResource.Id;
         }
     }
 }
