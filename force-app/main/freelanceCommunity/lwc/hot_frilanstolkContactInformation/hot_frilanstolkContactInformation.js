6c4710f128f6e14031b2a53270f2ffc501131f45
Lasse <lasse.wulff@nav.no>
2021-07-14 10:53:02 +0200

    added contactinfo edit component

---
+++ force-app/main/freelanceCommunity/lwc/hot_frilanstolkContactInformation/hot_frilanstolkContactInformation.js
@@ -0,0 +1,14 @@
+import { LightningElement, wire, track } from 'lwc';
+import getServiceResource from '@salesforce/apex/HOT_FreelanceUserInformationController.getServiceResource';
+
+export default class Hot_frilanstolkUserInformation extends LightningElement {
+    @track serviceResource;
+    @track recordId;
+    @wire(getServiceResource)
+    wiredGetServiceResource(result) {
+        if (result.data) {
+            this.serviceResource = result.data;
+            this.recordId = this.serviceResource.Id;
+        }
+    }
+}
