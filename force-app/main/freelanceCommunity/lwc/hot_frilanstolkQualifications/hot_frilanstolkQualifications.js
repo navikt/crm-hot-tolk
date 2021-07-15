4f60997d58de1ca1bce503253fd33041067f45cb
TonjeKaridotterOkland <55350586+TonjeKaridotterOkland@users.noreply.github.com>
2021-07-15 11:16:34 +0200

    Added comments for readability for the team
    Skillcontroller still not working

--- force-app/main/freelanceCommunity/lwc/hot_frilanstolkQualifications/hot_frilanstolkQualifications.js
+++ force-app/main/freelanceCommunity/lwc/hot_frilanstolkQualifications/hot_frilanstolkQualifications.js
@@ -2,10 +2,14 @@ import { LightningElement, wire, track } from 'lwc';
 import getServiceResourceSkill from '@salesforce/apex/HOT_FreelanceQualificationsController.getServiceResourceSkill';
 import getAllSkillsList from '@salesforce/apex/HOT_SkillController.getAllSkillsList';
 export default class Hot_frilanstolkQualifications extends LightningElement {
+    //tracking for serviceresourceskill
     @track serviceResourceSkill;
     @track recordId;
     @track serviceResourceSkillList;
+    //Tracking for Skills
     @track Id;
+    @track skillList;
+
     @wire(getServiceResourceSkill)
     wiredGetServiceResourceSkill(result) {
         if (result.data) {
@@ -13,8 +17,6 @@ export default class Hot_frilanstolkQualifications extends LightningElement {
             this.recordId = this.serviceResourceSkill.Id;
             this.serviceResourceSkillListFunction();
         }
-        //Denne kaller på en funksjon som henter ut alle skills unavhengig om serviceresource har noen skills eller ikke.
-        getAllSkillsList();
     }
     //Denne henter ut skillsene serviceresource har.
     serviceResourceSkillListFunction() {
@@ -24,7 +26,12 @@ export default class Hot_frilanstolkQualifications extends LightningElement {
         }
         this.serviceResourceSkillList = tempSRSkillList;
     }
-    //Denne henter ut alle skills som finnes. Denne fungerer ikke enda.
+
+    //TODO getAllSkillsList fungerer ikke. getAllSKillsList blir ikke importert fra controlleren. console.log blir ikke skrevet ut. @wire(GetAllSkillsList) gjør at hele komponenten forsvinner.
+
+    //Denne kaller på en funksjon som henter ut alle skills unavhengig om serviceresource har noen skills eller ikke. Jeg mistenker at det ikke er lov å bruke
+    //wire på to objekter i samme js fil, for når jeg brukte wire på begge funksjonene så forsvant hele komponenten.
+    // @wire(getAllSkillsList)
     getAllSkillsList(result2) {
         if (result2.data) {
             this.skill = result2.data;
@@ -34,7 +41,8 @@ export default class Hot_frilanstolkQualifications extends LightningElement {
         var tempSkillList = [];
         for (var i2 = 0; i2 < this.skill.length; i2++) {
             tempSkillList.push(this.skill[i2]);
-            console.log(tempSkillList);
+            console.log(tempSkillList() + ' console.loger hele arrayet med()');
+            console.log(tempSkillList + ' console.loger hele arrayet uten ()');
         }
         this.skillList = tempSkillList;
     }
