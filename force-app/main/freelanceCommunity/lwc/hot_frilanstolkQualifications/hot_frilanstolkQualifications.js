5afac1621141dffd52ae44d19d056c81fc2e56bf
vmjelde <vegard.mjelde@soprasteria.com>
2021-07-16 10:24:35 +0200

    Update hot_frilanstolkQualifications.js

--- force-app/main/freelanceCommunity/lwc/hot_frilanstolkQualifications/hot_frilanstolkQualifications.js
+++ force-app/main/freelanceCommunity/lwc/hot_frilanstolkQualifications/hot_frilanstolkQualifications.js
@@ -38,6 +38,12 @@ export default class Hot_frilanstolkQualifications extends LightningElement {
             this.Id = result2.Id;
         }
         //Er dette feil? sjekkker jeg lengden på arrayet, eller sjekker jeg lengden på et objekt, type at svaret er 1, fordi id =[0] og masterlabel=[1]?
+        //Nei. Dette fører til at metoden tryner. klasse-variablen "skill" er ikke definert, og du refererer til den her.
+        // siden skill er undefined feiler metoden når du prøver å hente ut length (this.skill.length)
+        //bruk let istedenfor var
+        //Du trenger ikke å legge til i en liste. result2.data ER en liste.
+        //Utenom det ser dette veldig bra ut! Du kan vurdere å ikke bruke record-view-form, men kanskje datatable?
+        //Du skal jo vise mer enn ett record. record-view-form fungerer best med kun ett record.
         var tempSkillList = [];
         for (var i2 = 0; i2 < this.skill.length; i2++) {
             tempSkillList.push(this.skill[i2]);
