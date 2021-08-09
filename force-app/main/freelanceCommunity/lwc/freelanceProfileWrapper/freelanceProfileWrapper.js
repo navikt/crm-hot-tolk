4be40281941803d961f8aa44942067ee21726049
Vemund Eggemoen <vemund.eggemoen@hotmail.com>
2021-08-09 11:03:26 +0200

    WIP move editProfile content

--- force-app/main/freelanceCommunity/lwc/freelanceProfileWrapper/freelanceProfileWrapper.js
+++ force-app/main/freelanceCommunity/lwc/freelanceProfileWrapper/freelanceProfileWrapper.js
@@ -1,14 +1,3 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement } from 'lwc';
 
-export default class FreelanceProfileWrapper extends LightningElement {
-    @track hot_frilanstolkEditProfile = false;
-    @track hot_frilanstolkUserInformation = true;
-    editProfile() {
-        this.hot_frilanstolkEditProfile = true;
-        this.hot_frilanstolkUserInformation = false;
-    }
-    updateProfile = () => {
-        this.hot_frilanstolkEditProfile = false;
-        this.hot_frilanstolkUserInformation = true;
-    };
-}
+export default class FreelanceProfileWrapper extends LightningElement {}
