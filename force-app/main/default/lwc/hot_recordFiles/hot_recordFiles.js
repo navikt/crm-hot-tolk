726b8141974a360b6e20a8f9eb06e6f08fbd8f35
eirikfladby <eirik.fladby@nav.no>
2021-09-29 15:39:21 +0200

    Changed recordFiles createBaseUrlLink to be async

--- force-app/main/default/lwc/hot_recordFiles/hot_recordFiles.js
+++ force-app/main/default/lwc/hot_recordFiles/hot_recordFiles.js
@@ -4,22 +4,18 @@ import createBaseUrlLink from '@salesforce/apex/HOT_RelatedFilesListController.c
 
 export default class hot_recordFiles extends LightningElement {
     @api recordId;
-    @track baseUrl;
-
-    connectedCallback() {
-        createBaseUrlLink().then((result) => {
-            this.baseUrl = result;
-        });
-    }
+    @api title;
 
     contentDocuments = [];
     contentDocumentsEmpty = true;
+
     @wire(getContentDocuments, { recordId: '$recordId' })
-    wiredgetContentDocuments(result) {
+    async wiredgetContentDocuments(result) {
         if (result.data) {
+            const url = await createBaseUrlLink();
             this.contentDocuments = result.data.map((item) => ({
                 ...item,
-                downloadLink: this.baseUrl + item.Id
+                downloadLink: url + item.Id
             }));
             this.contentDocumentsEmpty = this.contentDocuments.length === 0 ? true : false;
         } else {
