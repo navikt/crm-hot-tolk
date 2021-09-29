e43c907a5409b13a7cdd95f34eddd601b5c5ebcb
eirikfladby <eirik.fladby@nav.no>
2021-09-29 15:46:24 +0200

    recordfiles async and title

--- force-app/main/default/lwc/hot_recordFiles/hot_recordFiles.js
+++ force-app/main/default/lwc/hot_recordFiles/hot_recordFiles.js
@@ -1,25 +1,26 @@
-import { LightningElement, api, wire, track } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import getContentDocuments from '@salesforce/apex/HOT_RelatedFilesListController.getContentDocuments';
 import createBaseUrlLink from '@salesforce/apex/HOT_RelatedFilesListController.createBaseUrlLink';
+import { setDefaultValue } from 'c/componentHelperClass';
 
 export default class hot_recordFiles extends LightningElement {
     @api recordId;
-    @track baseUrl;
-
-    connectedCallback() {
-        createBaseUrlLink().then((result) => {
-            this.baseUrl = result;
-        });
-    }
+    @api title;
 
     contentDocuments = [];
     contentDocumentsEmpty = true;
+
+    get defaultTitle() {
+        return setDefaultValue(this.title, 'Vedlegg');
+    }
+
     @wire(getContentDocuments, { recordId: '$recordId' })
-    wiredgetContentDocuments(result) {
+    async wiredgetContentDocuments(result) {
         if (result.data) {
+            const url = await createBaseUrlLink();
             this.contentDocuments = result.data.map((item) => ({
                 ...item,
-                downloadLink: this.baseUrl + item.Id
+                downloadLink: url + item.Id
             }));
             this.contentDocumentsEmpty = this.contentDocuments.length === 0 ? true : false;
         } else {
