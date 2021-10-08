35aeb844bded338ef8c69e68b9be117ebbb38636
eirikfladby <eirik.fladby@nav.no>
2021-10-08 14:12:23 +0200

    clean file data after uploading too

--- force-app/main/userCommunity/lwc/hot_requestList/hot_requestList.js
+++ force-app/main/userCommunity/lwc/hot_requestList/hot_requestList.js
@@ -469,6 +469,7 @@ export default class RequestList extends NavigationMixin(LightningElement) {
         let detailPage = this.template.querySelector('.ReactModal__Overlay');
         detailPage.classList.add('hidden');
         this.showUploadFilesComponent = false;
+        this.clearFileData();
     }
 
     validateCheckbox() {
