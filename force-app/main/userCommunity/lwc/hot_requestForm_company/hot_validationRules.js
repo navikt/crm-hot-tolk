cfe0e1c9baccf18e3c59af55f1a12c1d397d1317
eirikfladby <eirik.fladby@nav.no>
2021-11-23 12:40:14 +0100

    removed unused export

--- force-app/main/userCommunity/lwc/hot_requestForm_company/hot_validationRules.js
+++ force-app/main/userCommunity/lwc/hot_requestForm_company/hot_validationRules.js
@@ -1,7 +1,6 @@
 import { require } from 'c/validationController';
 
 export let organizationNumberValidationRules = [validOrganizationNumber, require];
-export default { validOrganizationNumber };
 
 function validOrganizationNumber(organizationNumber) {
     let regExp = RegExp('\\d{9}');
