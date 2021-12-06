f9bf73a0f5d680622475f64df08b2d089b0f7a55
vmjelde <vegard.mjelde@soprasteria.com>
2021-12-06 15:23:08 +0100

    fixed functionality

--- force-app/main/userCommunity/lwc/hot_recurringTimeInput/hot_recurringTimeInput_validationRules.js
+++ force-app/main/userCommunity/lwc/hot_recurringTimeInput/hot_recurringTimeInput_validationRules.js
@@ -1,45 +1,26 @@
-export let startDateValidations = [dateInPast];
-export let endTimeValidations = [startBeforeEnd];
-export let recurringDaysValidations = [requireDaysBasedOnRecurringType];
-export let recurringEndDateValidations = [
-    startDateBeforeRecurringEndDate,
-    restrictTheNumberOfDays,
-    chosenDaysWithinPeriod
-];
-
 let nowDate = new Date();
 
-function dateInPast(date) {
-    if (date == null || date == '') {
-        return '';
-    }
+export function dateInPast(date) {
     date = new Date(date);
     let today = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 0, 0, 0, 0);
     return date.getTime() < today.getTime() ? 'Du kan ikke bestille tid i fortiden' : '';
 }
 
-function startBeforeEnd(endDate, startDate) {
+export function startBeforeEnd(endDate, startDate) {
     startDate = new Date(startDate);
     endDate = new Date(endDate);
     return startDate.getTime() > endDate.getTime() ? 'Start tid må være før slutt tid.' : '';
 }
 
-function requireDaysBasedOnRecurringType(type, days, recurringEndDate, startDate) {
-    if ((type === 'Weekly' || type === 'Biweekly') && days.length === 0) {
-        return 'Du må velge minst én dag tolkebehovet gjentas.';
-    }
-    return '';
-}
-
-function startDateBeforeRecurringEndDate(type, days, recurringEndDate, startDate) {
+export function startDateBeforeRecurringEndDate(recurringEndDate, startDate) {
     return new Date(startDate).getTime() > new Date(recurringEndDate) ? 'Slutt dato må være etter start dato' : '';
 }
-function restrictTheNumberOfDays(type, days, recurringEndDate, startDate) {
+export function restrictTheNumberOfDays(recurringEndDate, startDate) {
     return new Date(recurringEndDate) - new Date(startDate).getTime() > 199 * 24 * 3600000 && startDate != null
         ? 'Du kan ikke legge inn gjentagende bestilling med en varighet på over 6 måneder'
         : '';
 }
-function chosenDaysWithinPeriod(type, days, recurringEndDate, startDate) {
+export function chosenDaysWithinPeriod(type, days, recurringEndDate, startDate) {
     if (type === 'Daily') {
         return '';
     }
