98f6df94e3f07cabb7432d4c1d4f100161e015de
vmjelde <vegard.mjelde@soprasteria.com>
2021-12-17 13:36:15 +0100

    Update listFiltersButton.js

--- force-app/main/default/lwc/listFiltersButton/listFiltersButton.js
+++ force-app/main/default/lwc/listFiltersButton/listFiltersButton.js
@@ -7,4 +7,8 @@ export default class ListFiltersButton extends LightningElement {
     openFilters() {
         this.template.querySelector('c-list-filters').openFilters();
     }
+    applyFilter(event) {
+        const eventToSend = new CustomEvent('applyfilter', { detail: event.detail });
+        this.dispatchEvent(eventToSend);
+    }
 }
