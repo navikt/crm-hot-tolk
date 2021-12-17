61ce2b13bd8b414253cd382044af464a77bf3553
vmjelde <vegard.mjelde@soprasteria.com>
2021-12-17 13:34:55 +0100

    filter icon button

--- force-app/main/default/lwc/icon/icon.js
+++ force-app/main/default/lwc/icon/icon.js
@@ -5,11 +5,13 @@ export default class Icon extends LightningElement {
     @api icon;
     @api ariaLabel;
     @api fill;
+    @api svgStyle;
+
     get iconToShow() {
         let fill = this.fill !== undefined ? this.fill : '';
         return IKONER + '/' + this.icon + '/' + this.icon + fill + '.svg#' + this.icon + fill;
     }
     get defaultStyle() {
-        return 'width: 24px; height: 24px';
+        return this.svgStyle === undefined ? 'width: 24px; height: 24px' : this.svgStyle;
     }
 }
