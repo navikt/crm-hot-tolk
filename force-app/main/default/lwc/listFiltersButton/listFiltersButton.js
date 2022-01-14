db0f11341b2ba1ef20245f8691a24db6ad9f39f6
eirikfladby <eirik.fladby@nav.no>
2022-01-14 16:03:48 +0100

    set filter button style when active on desktop

--- force-app/main/default/lwc/listFiltersButton/listFiltersButton.js
+++ force-app/main/default/lwc/listFiltersButton/listFiltersButton.js
@@ -4,9 +4,24 @@ export default class ListFiltersButton extends LightningElement {
     @api header;
     @api activeFilters;
     @api filters;
+    isOpen = false;
+
+    connectedCallback() {
+        document.documentElement.style.setProperty('--filterButtonColor', '#262626');
+        document.documentElement.style.setProperty('--filterButtonBackgroundColor', '#EFEFEF');
+        document.documentElement.style.setProperty('--filterButtonBorderColor', '#929292');
+    }
 
     openFilters() {
         if (window.screen.width > 576) {
+            this.isOpen = !this.isOpen;
+            if (this.isOpen) {
+                document.documentElement.style.setProperty('--filterButtonColor', '#ffffff');
+                document.documentElement.style.setProperty('--filterButtonBackgroundColor', '#0056b4');
+            } else {
+                document.documentElement.style.setProperty('--filterButtonColor', '#262626');
+                document.documentElement.style.setProperty('--filterButtonBackgroundColor', '#EFEFEF');
+            }
             const eventToSend = new CustomEvent('opendesktopfilters', { detail: 'opendesktopfilters' });
             this.dispatchEvent(eventToSend);
         } else {
