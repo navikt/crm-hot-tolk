da9713699c4bf0bd981eb589203f91dc98b72de0
eirikfladby <eirik.fladby@nav.no>
2022-01-14 12:34:07 +0100

    fixed screen reader for table and added tooltip on icon hover

--- force-app/main/default/lwc/table/table.js
+++ force-app/main/default/lwc/table/table.js
@@ -9,7 +9,7 @@ export default class Table extends LightningElement {
     recordMap = {};
     get recordsToShow() {
         let records = [];
-        //console.log('this.records: ', JSON.stringify(this.records));
+        console.log('this.records: ', JSON.stringify(this.records));
         if (this.records !== undefined && this.records !== null) {
             for (let record of this.records) {
                 let fields = [];
@@ -30,6 +30,7 @@ export default class Table extends LightningElement {
                 this.recordMap[record.Id] = record;
             }
         }
+        console.log('recordsToShow: ', records);
         return records;
     }
 
