0b10ccc9a98e58db7a213a28c1ff07fc944d5cae
eirikfladby <eirik.fladby@gmail.com>
2022-03-23 13:05:29 +0100

    update  dependencies

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -10,8 +10,8 @@ call sfdx force:package:install --package 04t7U000000ToLcQAK -r -k %3 --wait 10 
 echo "Installerer crm-platform-access-control ver. 0.83"
 call sfdx force:package:install --package 04t7U000000TnyOQAS -r -k %3 --wait 10 --publishwait 10
 
-echo "Installerer crm-community-base ver. 0.56"
-call sfdx force:package:install --package 04t7U000000TobkQAC -r -k %3 --wait 10 --publishwait 10
+echo "Installerer crm-community-base ver. 0.58"
+call sfdx force:package:install --package 04t7U000000TohOQAS -r -k %3 --wait 10 --publishwait 10
 
 echo "Installerer crm-platform-reporting ver. 0.25"
 call sfdx force:package:install --package 04t2o000000ySIYAA2 -r -k %3 --wait 10 --publishwait 10
