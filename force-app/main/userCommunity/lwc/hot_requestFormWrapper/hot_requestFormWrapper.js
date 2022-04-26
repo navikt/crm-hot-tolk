3c8bcccfb86d1127e00ac0b8ef2ac2000be516d6
eirikfladby <eirik.fladby@gmail.com>
2022-04-26 11:04:59 +0200

    added more clear indication of loading when sending a request

--- force-app/main/userCommunity/lwc/hot_requestFormWrapper/hot_requestFormWrapper.js
+++ force-app/main/userCommunity/lwc/hot_requestFormWrapper/hot_requestFormWrapper.js
@@ -57,6 +57,7 @@ export default class Hot_requestFormWrapper extends NavigationMixin(LightningEle
         if (!hasErrors) {
             this.promptOverlap().then((overlapOk) => {
                 if (overlapOk) {
+                    this.hideFormAndShowLoading();
                     this.submitForm();
                 } else {
                     this.spin = false;
@@ -137,8 +138,9 @@ export default class Hot_requestFormWrapper extends NavigationMixin(LightningEle
 
     handleAlertDialogClick(event) {
         if (event.detail === 'confirm' && this.modalHeader === 'Du har allerede bestillinger i dette tidsrommet.') {
-            this.submitForm();
             this.spin = true;
+            this.hideFormAndShowLoading();
+            this.submitForm();
         }
     }
 
@@ -172,9 +174,16 @@ export default class Hot_requestFormWrapper extends NavigationMixin(LightningEle
     }
 
     hideFormAndShowSuccess() {
+        this.template.querySelector('.submitted-loading').classList.add('hidden');
+        this.template.querySelector('.submitted-false').classList.add('hidden');
         this.template.querySelector('.submitted-true').classList.remove('hidden');
         this.template.querySelector('.h2-successMessage').focus();
+    }
+
+    hideFormAndShowLoading() {
         this.template.querySelector('.submitted-false').classList.add('hidden');
+        this.template.querySelector('.submitted-loading').classList.remove('hidden');
+        this.template.querySelector('.h2-loadingMessage').focus();
     }
 
     uploadFiles() {
