2d2140cf8b2d93f79ce3a123d8fc9caf83720177
vmjelde <vegard.mjelde@soprasteria.com>
2022-04-26 10:23:18 +0200

    added support for commaSeperatedList

--- force-app/main/default/lwc/hot_interpreters/hot_interpreters.js
+++ force-app/main/default/lwc/hot_interpreters/hot_interpreters.js
@@ -1,33 +1,26 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import getInterpreters from '@salesforce/apex/HOT_InterpreterOnWorkOrderService.getInterpreters';
-import getInterpreters2 from '@salesforce/apex/HOT_InterpreterOnWorkOrderService.getInterpreters2';
 
 export default class Hot_interpreters extends LightningElement {
-    @api objectName = 'WorkOrder';
-    @api mergeField = 'Id';
+    @api objectName;
+    @api mergeField;
     @api recordId;
-    @api workOrderId;
-    @api hideHtml;
-    @track interpreters = [];
+    @api commaSeperatedList = false;
+    @track interpreters;
 
-    //@wire(getInterpreters, { recordId: '$recordId', mergeField: '$mergeField', objectName: '$objectName' })
-    //wirdedGetInterpreters(result) {
-    //    console.log(JSON.stringify(result));
-    //    if (result.data) {
-    //        this.interpreters = result.data;
-    //    }
-    //}
-    connectedCallback() {
-        console.log(this.mergeField);
-        console.log(this.recordId);
-        console.log(this.interpreters);
-    }
-
-    @wire(getInterpreters2, { workOrderId: '$workOrderId' })
-    wirdedGetInterpreters2(result) {
-        console.log(JSON.stringify(result));
+    @wire(getInterpreters, { recordId: '$recordId', mergeField: '$mergeField', objectName: '$objectName' })
+    wirdedGetInterpreters(result) {
         if (result.data) {
-            this.interpreters = result.data;
+            if (this.commaSeperatedList) {
+                let interpreters = '';
+                for (let interpreter of result.data) {
+                    interpreters += interpreter + ', ';
+                }
+                interpreters = interpreters.slice(0, -2);
+                this.interpreters = interpreters;
+            } else {
+                this.interpreters = result.data;
+            }
         }
     }
 }
