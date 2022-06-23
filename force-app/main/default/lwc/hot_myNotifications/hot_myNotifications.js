4a49e742ade64d94299563f4dda398162f5d65ad
vmjelde <vegard.mjelde@soprasteria.com>
2022-06-23 13:13:45 +0200

    updated return value and added number of unreads

--- force-app/main/default/lwc/hot_myNotifications/hot_myNotifications.js
+++ force-app/main/default/lwc/hot_myNotifications/hot_myNotifications.js
@@ -2,22 +2,17 @@ import { LightningElement, track, wire, api } from 'lwc';
 import getNotifications from '@salesforce/apex/HOT_UserNotification.getNotifications';
 
 export default class Hot_myNotifications extends LightningElement {
-    @wire(getNotifications, { notificationTypeNames: '' })
-    handleWire(result) {
-        if (result.data) {
-            this.notifications = result.data;
-            console.log('All notifications ', this.notifications.length);
-            console.log('notificatiuon ', JSON.stringify(this.notifications[0]));
-        }
-    }
+    @track notificationClass = {};
     @track notifications = [];
     @api notificationTypes = '';
 
     @wire(getNotifications, { notificationTypeNames: '$notificationTypes' })
     handlegetNotifications(result) {
+        console.log(result);
+        console.log(JSON.stringify(result));
         if (result.data) {
-            this.res = result.data;
-            console.log('only user notifications ', this.res.length);
+            this.notificationClass = result.data;
+            this.notifications = result.data.notifications;
         }
     }
 
