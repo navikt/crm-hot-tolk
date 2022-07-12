7337c6a77bb77b5440ae6f3f6fd34307802b3f1f
vmjelde <vegard.mjelde@soprasteria.com>
2022-07-12 10:08:09 +0200

    dep

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -11,7 +11,7 @@ echo "Installerer crm-platform-access-control ver. 0.90"
 call sfdx force:package:install --package 04t7U000000Tp3jQAC -r -k %3 --wait 10 --publishwait 10
 
 echo "Installerer crm-community-base ver. 0.67"
-call sfdx force:package:install --package 04t7U000000TpwoQAC -r -k %3 --wait 10 --publishwait 10
+call sfdx force:package:install --package 04t7U000000TpyuQAC -r -k %3 --wait 10 --publishwait 10
 
 echo "Installerer crm-platform-reporting ver. 0.25"
 call sfdx force:package:install --package 04t2o000000ySIYAA2 -r -k %3 --wait 10 --publishwait 10
