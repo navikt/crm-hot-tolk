1387d39f66b48bcd0917377c9dbaabb7d8d03d1a
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2022-09-30 11:56:23 +0200

    2

--- force-app/main/userCommunity/lwc/hot_tolketjenestenUserInformation/hot_tolketjenestenUserInformation.js
+++ force-app/main/userCommunity/lwc/hot_tolketjenestenUserInformation/hot_tolketjenestenUserInformation.js
@@ -1,23 +1,31 @@
-import { LightningElement, wire, track, api } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getPerson from '@salesforce/apex/HOT_UserInformationController.getPerson';
 import getNotificationPickListValues from '@salesforce/apex/HOT_UserInformationController.getNotificationPickListValues';
+import changeUserNotificationsetting from '@salesforce/apex/HOT_UserInformationController.changeUserNotificationsetting';
 
 export default class hot_tolketjenestenUserInformation extends LightningElement {
     @track person;
     @track recordId;
     @track options;
     @track selectedOption;
+    @track testtest;
     @wire(getPerson)
     wiredGetPerson(result) {
         if (result.data) {
             this.person = result.data;
             this.recordId = this.person.Id;
             this.selectedOption = this.person.HOT_NotificationChannel__c;
+            getNotificationPickListValues({
+                chosen: this.selectedOption
+            }).then((data) => {
+                this.options = data;
+            });
         }
-        getNotificationPickListValues({
-            chosen: this.selectedOption
-        }).then((data) => {
-            this.options = data;
-        });
+    }
+    selectionChangeHandler() {
+        alert('Du endret til: ' + event.target.value + ' Personid: ' + this.recordId);
+        const data = '';
+        data = changeUserNotificationsetting({ personId: this.recordId, newNotificationValue: event.target.value });
+        alert(data);
     }
 }
