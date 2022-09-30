cfe02dae7d38bade7338a6e941a4b80354ea9be5
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2022-09-30 07:49:42 +0200

    1

--- force-app/main/userCommunity/lwc/hot_tolketjenestenUserInformation/hot_tolketjenestenUserInformation.js
+++ force-app/main/userCommunity/lwc/hot_tolketjenestenUserInformation/hot_tolketjenestenUserInformation.js
@@ -1,14 +1,17 @@
-import { LightningElement, wire, track } from 'lwc';
+import { LightningElement, wire, track, api } from 'lwc';
 import getPerson from '@salesforce/apex/HOT_UserInformationController.getPerson';
 
 export default class hot_tolketjenestenUserInformation extends LightningElement {
     @track person;
     @track recordId;
+    @track options;
+    @track selectedOption;
     @wire(getPerson)
     wiredGetPerson(result) {
         if (result.data) {
             this.person = result.data;
             this.recordId = this.person.Id;
+            this.selectedOption = this.person.HOT_NotificationChannel__c;
         }
     }
 }
