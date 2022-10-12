f4def50a8791f5ffe5b47dee71ab44ce17b15c4f
Jan Robert Gjøstøl <98319833+JanRobertNav@users.noreply.github.com>
2022-10-12 09:11:43 +0200

    .
    la til felt for isKrrQueued, med conditional if:true rendering p-tag og startet på funksjon updateKrrStatusToQueued i apex

--- force-app/main/userCommunity/lwc/hot_userContactInformation/hot_userContactInformation.js
+++ force-app/main/userCommunity/lwc/hot_userContactInformation/hot_userContactInformation.js
@@ -1,14 +1,29 @@
 import { LightningElement, wire, track } from 'lwc';
-import getPersonPhoneEmail from '@salesforce/apex/HOT_UserInformationController.getPersonPhoneEmail';
+import getPersonPhoneEmailAndStatus from '@salesforce/apex/HOT_UserInformationController.getPersonPhoneEmailAndStatus';
+import INT_KrrIntegrationStatus__c from '@salesforce/schema/Person__ChangeEvent.INT_KrrIntegrationStatus__c';
+import updateKrrStatusToQueued from '@salesforce/apex/HOT_UserInformationController.updateKrrStatusToQueued';
 
 export default class hot_userContactInformation extends LightningElement {
     @track person;
     @track recordId;
-    @wire(getPersonPhoneEmail)
+    @wire(getPersonPhoneEmailAndStatus)
     wiredPerson(result) {
         if (result.data) {
             this.person = result.data;
             this.recordId = this.person.Id;
         }
     }
+
+
+    setKrrIntegrationStatusToQueued() {
+        //alert('test, integrasjonsstatus: ' );
+        //INT_KrrIntegrationStatus__c = 'Queued';
+        this.person.INT_KrrIntegrationStatus__c = 'Queued';
+        updateKrrStatusToQueued(this.person)
+    }
+
+    get isKrrQueued (){
+        return INT_KrrIntegrationStatus__c == 'Queued' ? true : false;
+    }
+
 }
