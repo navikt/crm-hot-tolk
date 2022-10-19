93f23ae33795fdd4e90dbe79a94ed299fa5f2a2e
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2022-10-19 07:16:21 +0200

    litt rydding og bytte tilbake

--- force-app/main/default/lwc/hot_isDeceasedBannerRequest/hot_isDeceasedBannerRequest.js
+++ force-app/main/default/lwc/hot_isDeceasedBannerRequest/hot_isDeceasedBannerRequest.js
@@ -1,15 +1,15 @@
 import { LightningElement, wire, api, track } from 'lwc';
-//import { getRecord } from 'lightning/uiRecordApi';
 import isDeceased from '@salesforce/apex/HOT_RequestController.isUserDeaceased';
 
 export default class Hot_isDeceasedBannerRequest extends LightningElement {
     @track isDeceased;
     @api recordId;
 
-    @wire(isDeceased)
-    wiredIsDeceased(result) {
-        if (result.data) {
+    connectedCallback() {
+        isDeceased({
+            requestId: this.recordId
+        }).then((data) => {
             this.isDeceased = data;
-        }
+        });
     }
 }
