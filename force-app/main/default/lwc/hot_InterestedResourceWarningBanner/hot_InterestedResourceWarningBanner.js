77064ed3d9ba0f1f202eb18833a2386aaf436f73
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2022-11-22 11:07:59 +0100

    Update hot_InterestedResourceWarningBanner.js

--- force-app/main/default/lwc/hot_InterestedResourceWarningBanner/hot_InterestedResourceWarningBanner.js
+++ force-app/main/default/lwc/hot_InterestedResourceWarningBanner/hot_InterestedResourceWarningBanner.js
@@ -5,7 +5,6 @@ import { formatDate } from 'c/datetimeFormatter';
 export default class Hot_InterestedResourceWarningBanner extends LightningElement {
     @api recordId;
     @track serviceAppointments = [];
-    @track serviceAppointments2 = [];
     @track hasOverlap = false;
     oppdragsInfo = '';
 
@@ -13,63 +12,33 @@ export default class Hot_InterestedResourceWarningBanner extends LightningElemen
         let recordId = this.recordId;
 
         checkForOverlap({ recordId: recordId }).then((data) => {
-            this.serviceAppointments = data;
-            if (this.serviceAppointments.length != 0) {
-                this.hasOverlap = true;
-                // this.serviceAppointments.forEach((element) => {
-                //     let startTimeFormatted = new Date(element.SchedStartTime);
-                //     let endTimeFormatted = new Date(element.SchedEndTime);
-
-                //     this.oppdragsInfo +=
-                //         element.AppointmentNumber +
-                //         ' \n ' +
-                //         startTimeFormatted.getDate() +
-                //         '.' +
-                //         startTimeFormatted.getMonth() +
-                //         '.' +
-                //         startTimeFormatted.getFullYear() +
-                //         ' ' +
-                //         startTimeFormatted.getHours() +
-                //         ':' +
-                //         endTimeFormatted.getMinutes() +
-                //         ' - ' +
-                //         endTimeFormatted.getDate() +
-                //         '.' +
-                //         endTimeFormatted.getMonth() +
-                //         '.' +
-                //         endTimeFormatted.getFullYear() +
-                //         ' ' +
-                //         endTimeFormatted.getHours() +
-                //         ':' +
-                //         endTimeFormatted.getMinutes();
-                // });
-                for (let record of data) {
-                    let startTimeFormatted = new Date(record.SchedStartTime);
-                    let endTimeFormatted = new Date(record.SchedEndTime);
-                    record.Link = '/lightning/r/' + record.Id + '/view';
-                    record.oppdragsInfo =
-                        startTimeFormatted.getDate() +
-                        '.' +
-                        startTimeFormatted.getMonth() +
-                        '.' +
-                        startTimeFormatted.getFullYear() +
-                        ' ' +
-                        startTimeFormatted.getHours() +
-                        ':' +
-                        endTimeFormatted.getMinutes() +
-                        ' - ' +
-                        endTimeFormatted.getDate() +
-                        '.' +
-                        endTimeFormatted.getMonth() +
-                        '.' +
-                        endTimeFormatted.getFullYear() +
-                        ' ' +
-                        endTimeFormatted.getHours() +
-                        ':' +
-                        endTimeFormatted.getMinutes();
-                    this.serviceAppointments2.push(record);
-                }
+            for (let record of data) {
+                let startTimeFormatted = new Date(record.SchedStartTime);
+                let endTimeFormatted = new Date(record.SchedEndTime);
+                record.Link = '/lightning/r/' + record.Id + '/view';
+                record.oppdragsInfo =
+                    startTimeFormatted.getDate() +
+                    '.' +
+                    startTimeFormatted.getMonth() +
+                    '.' +
+                    startTimeFormatted.getFullYear() +
+                    ' ' +
+                    startTimeFormatted.getHours() +
+                    ':' +
+                    endTimeFormatted.getMinutes() +
+                    ' - ' +
+                    endTimeFormatted.getDate() +
+                    '.' +
+                    endTimeFormatted.getMonth() +
+                    '.' +
+                    endTimeFormatted.getFullYear() +
+                    ' ' +
+                    endTimeFormatted.getHours() +
+                    ':' +
+                    endTimeFormatted.getMinutes();
+                this.serviceAppointments.push(record);
             }
+            this.hasOverlap = this.serviceAppointments.length > 0;
         });
     }
 }
