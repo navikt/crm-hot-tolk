f60e1a2089dfdf8d70d864b06746b86291527513
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2023-01-11 13:14:07 +0100

    :)

--- force-app/main/dialogue/external-components/lwc/hot_threadList/hot_threadList.js
+++ force-app/main/dialogue/external-components/lwc/hot_threadList/hot_threadList.js
@@ -44,7 +44,42 @@ export default class Hot_threadList extends NavigationMixin(LightningElement) {
             svg: true
         }
     ];
+    @track tabs = [
+        { name: 'mythreads', label: 'Samtaler med formidler', selected: true },
+        { name: 'interpreterthreads', label: 'Samtaler tolk-bruker', selected: false }
+    ];
 
+    @track tabMap = {
+        mythreads: true,
+        interpreterthreads: false
+    };
+
+    setActiveTabMobile(event) {
+        this.setActiveTab({ target: { dataset: { id: event.detail.name } } });
+    }
+
+    setActiveTab(event) {
+        this.updateTab(event);
+    }
+    updateTab(event) {
+        for (let tab of this.tabs) {
+            tab.selected = false;
+            this.tabMap[tab.name] = false;
+            if (tab.name === event.target.dataset.id) {
+                tab.selected = true;
+                this.activeTab = tab.name;
+                this.tabMap[tab.name] = true;
+                if (this.activeTab == 'mythreads') {
+                    this.showMythreads = true;
+                    this.showInterpreterthreads = false;
+                } else {
+                    this.showMythreads = false;
+                    this.showInterpreterthreads = true;
+                }
+            }
+        }
+        this.updateTabStyle();
+    }
     openThread(event) {
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
@@ -55,9 +90,13 @@ export default class Hot_threadList extends NavigationMixin(LightningElement) {
             }
         });
     }
-
-    @track threads = [];
+    @track unmappedThreads;
+    @track threads;
+    @track unmappedInterpreterThreads;
+    showMythreads = true;
+    showInterpreterthreads = false;
     noThreads = false;
+    noInterpreterThreads = false;
     wiredThreadsResult;
     @wire(getMyThreads)
     wiredThreads(result) {
@@ -66,18 +105,40 @@ export default class Hot_threadList extends NavigationMixin(LightningElement) {
             getContactId({})
                 .then((contactId) => {
                     this.userContactId = contactId;
-                    this.threads = result.data.map((x) => ({
+                    this.unmappedThreads = [];
+                    this.threads = [];
+                    this.unmappedInterpreterThreads = [];
+                    result.data.forEach((element) => {
+                        console.log(element.Id);
+                        if (element.CRM_Type__c == 'HOT_BRUKER-TOLK') {
+                            this.unmappedInterpreterThreads.push(element);
+                        }
+                        if (element.CRM_Type__c == 'HOT_BRUKER-FORMIDLER') {
+                            this.unmappedThreads.push(element);
+                        }
+                    });
+                    this.threads = this.unmappedThreads.map((x) => ({
                         ...x,
                         read:
                             x.CRM_Number_of_unread_Messages__c > 0 && x.HOT_Last_message_from__c != this.userContactId
                                 ? false
                                 : true
                     }));
+                    this.interpreterThreads = this.unmappedInterpreterThreads.map((x) => ({
+                        ...x,
+                        read:
+                            x.CRM_Number_of_unread_Messages__c > 0 && x.HOT_Last_message_from__c != this.userContactId
+                                ? false
+                                : true
+                    }));
+                    // console.log(result.data);
                     this.noThreads = this.threads.length === 0;
+                    this.noInterpreterThreads = this.interpreterThreads.length === 0;
                 })
                 .catch((error) => {
                     //Apex error
                 });
+            this.activeTab = 'mythreads';
         }
     }
 
@@ -87,6 +148,17 @@ export default class Hot_threadList extends NavigationMixin(LightningElement) {
 
     connectedCallback() {
         refreshApex(this.wiredThreadsResult);
+    }
+    renderedCallback() {
+        this.updateTabStyle();
+    }
+    updateTabStyle() {
+        this.template.querySelectorAll('button.tab').forEach((element) => {
+            element.classList.remove('tab_active');
+            if (element.dataset.id === this.activeTab) {
+                element.classList.add('tab_active');
+            }
+        });
     }
 
     goBack() {
