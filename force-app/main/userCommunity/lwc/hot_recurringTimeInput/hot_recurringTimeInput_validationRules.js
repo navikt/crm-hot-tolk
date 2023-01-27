444bf26e5e35ee66a8db96273b1f58b6c50ba614
Edvard Brekke <edvard_brekke@me.com>
2023-01-27 13:10:14 +0100

    Update hot_recurringTimeInput_validationRules.js

--- force-app/main/userCommunity/lwc/hot_recurringTimeInput/hot_recurringTimeInput_validationRules.js
+++ force-app/main/userCommunity/lwc/hot_recurringTimeInput/hot_recurringTimeInput_validationRules.js
@@ -10,12 +10,6 @@ export function dateInPast(date) {
     return date.getTime() < today.getTime() ? 'Du kan ikke bestille tid i fortiden' : '';
 }
 
-export function endAfterStart(startDate, endDate) {
-    startDate = new Date(startDate);
-    endDate = new Date(endDate);
-    return endDate.getTime() === startDate.getTime() ? 'Sluttid må være etter starttid.' : '';
-}
-
 export function startBeforeEnd(endDate, startDate) {
     startDate = new Date(startDate);
     endDate = new Date(endDate);
