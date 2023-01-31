e5d8141bf5cc9e3fa5e66393303ce9bc3ee0500d
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2023-01-31 13:13:59 +0100

    Mine oppdrag

--- force-app/main/freelanceCommunity/lwc/hot_myServiceAppointments/hot_myServiceAppointments.js
+++ force-app/main/freelanceCommunity/lwc/hot_myServiceAppointments/hot_myServiceAppointments.js
@@ -104,7 +104,6 @@ export default class Hot_myServiceAppointments extends LightningElement {
 
     refresh() {
         this.filters = defaultFilters();
-        this.goToRecordDetails({ detail: { Id: this.recordId } });
         this.sendRecords();
         this.sendFilters();
         this.applyFilter({ detail: { filterArray: this.filters, setRecords: true } });
@@ -125,7 +124,8 @@ export default class Hot_myServiceAppointments extends LightningElement {
     isSeries = false;
     showTable = true;
     goToRecordDetails(result) {
-        window.scrollTo(0, 0);
+        this.template.querySelector('.serviceAppointmentDetails').classList.remove('hidden');
+        this.template.querySelector('.serviceAppointmentDetails').focus();
         let today = new Date();
         this.serviceAppointment = undefined;
         this.interestedResource = undefined;
@@ -145,7 +145,7 @@ export default class Hot_myServiceAppointments extends LightningElement {
             }
         }
         this.updateURL();
-        this.sendDetail();
+        //this.sendDetail();
     }
 
     @api recordId;
@@ -213,6 +213,9 @@ export default class Hot_myServiceAppointments extends LightningElement {
                 value: this.recordId
             }
         ];
+    }
+    closeModal() {
+        this.template.querySelector('.serviceAppointmentDetails').classList.add('hidden');
     }
     handleStatusChange(event) {
         console.log('handleStatusChange', event.detail);
