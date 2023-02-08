7abcde4611de7a7ebd247c302e781cc3b7cb3a6c
Edvard Brekke <edvard_brekke@me.com>
2023-02-08 16:04:33 +0100

    .

--- force-app/main/userCommunity/lwc/hot_requestForm_user/hot_requestForm_user.js
+++ force-app/main/userCommunity/lwc/hot_requestForm_user/hot_requestForm_user.js
@@ -67,12 +67,15 @@ export default class Hot_requestForm_user extends LightningElement {
 
     @api parentFieldValues;
     connectedCallback() {
-        document.body.scrollIntoView({ behavior: 'smooth', block: 'start' });
         for (let field in this.parentFieldValues) {
             if (this.fieldValues[field] != null) {
                 this.fieldValues[field] = this.parentFieldValues[field];
             }
         }
         this.setRadiobuttonsOnConnected();
+    }
+
+    renderedCallback() {
+        this.template.querySelector('h2').focus();
     }
 }
