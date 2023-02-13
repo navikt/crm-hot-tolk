d27054ae4baebce79ad0a251e3279ec491b376b5
Edvard Brekke <edvard_brekke@me.com>
2023-02-13 20:18:51 +0100

    Update hot_requestForm_user.js

--- force-app/main/userCommunity/lwc/hot_requestForm_user/hot_requestForm_user.js
+++ force-app/main/userCommunity/lwc/hot_requestForm_user/hot_requestForm_user.js
@@ -77,8 +77,4 @@ export default class Hot_requestForm_user extends LightningElement {
         }
         this.setRadiobuttonsOnConnected();
     }
-
-    // renderedCallback() {
-    //     this.template.querySelector('h2').focus();
-    // }
 }
