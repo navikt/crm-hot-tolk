1714c85dfa09e986b24859d01e56aaa6275f7d1c
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2023-05-04 07:52:29 +0200

    :I

--- force-app/main/default/lwc/hot_registerDispatcherReadComment/hot_registerDispatcherReadComment.js
+++ force-app/main/default/lwc/hot_registerDispatcherReadComment/hot_registerDispatcherReadComment.js
@@ -1,5 +1,11 @@
 import { LightningElement, api, wire } from 'lwc';
+import dispatcherReadComment from '@salesforce/apex/HOT_InterestedResourcesListController.dispatcherReadComment';
 
 export default class Hot_registerDispatcherReadComment extends LightningElement {
-    //slette denne?
+    @api recordId;
+
+    connectedCallback() {
+        let recordId = this.recordId;
+        dispatcherReadComment({ recordId });
+    }
 }
