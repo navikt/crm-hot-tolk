805f06027e64962a4500cb298aab9e718a360b9b
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2023-06-09 07:31:26 +0200

    :)

--- force-app/main/default/lwc/hot_freelanceHome/hot_freelanceHome.js
+++ force-app/main/default/lwc/hot_freelanceHome/hot_freelanceHome.js
@@ -1,8 +1,16 @@
 import { LightningElement, track, wire } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
+import ICON_Person from '@salesforce/resourceUrl/TINDPersonIcon';
+import ICON_Chat from '@salesforce/resourceUrl/Chat2Icon';
+import ICON_Tasklist from '@salesforce/resourceUrl/TaskList';
+import ICON_RightArrow from '@salesforce/resourceUrl/rightArrow';
 
 export default class Hot_freelanceHome extends NavigationMixin(LightningElement) {
     @track pageLinks = {};
+    personIcon = ICON_Person;
+    tasklistIcon = ICON_Tasklist;
+    chatIcon = ICON_Chat;
+    rightArrow = ICON_RightArrow;
     connectedCallback() {
         sessionStorage.clear(); // Clear session storage when on home
         window.scrollTo(0, 0);
