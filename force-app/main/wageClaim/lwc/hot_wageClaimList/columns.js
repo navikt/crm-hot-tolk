069a3e2ba94f2bad349742f74704ef8fdb93c82f
Edvard Brekke <edvard_brekke@me.com>
2023-08-01 11:02:45 +0200

    WC list etc

--- force-app/main/wageClaim/lwc/hot_wageClaimList/columns.js
+++ force-app/main/wageClaim/lwc/hot_wageClaimList/columns.js
@@ -5,8 +5,13 @@ export let columns = [
         type: 'Datetime'
     },
     {
-        label: 'Døv/Døvblind',
-        name: 'DegreeOfHearingAndVisualImpairment__c',
+        label: 'Poststed',
+        name: 'ServiceAppointmentCity__c',
+        type: 'String'
+    },
+    {
+        label: 'Oppdragsnummer',
+        name: 'ServiceAppointmentName__c',
         type: 'String'
     },
     {
@@ -15,12 +20,7 @@ export let columns = [
         type: 'String'
     },
     {
-        label: 'Poststed',
-        name: 'ServiceAppointmentCity__c',
-        type: 'String'
-    },
-    {
-        label: 'Arbeidstype',
+        label: 'Tolkemetode',
         name: 'WorkTypeName__c',
         type: 'String'
     },
