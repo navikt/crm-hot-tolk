4e399e436c8c3068e6b4d6fcd3f2b679d12529cc
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2023-10-12 07:30:27 +0200

    Banner link til forsiden

---
+++ force-app/main/default/lwc/hot_notificationCentreLink/hot_notificationCentreLink.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import ICON_Bell from '@salesforce/resourceUrl/Bell';
+
+export default class Hot_notificationCentreLink extends LightningElement {
+    bellIcon = ICON_Bell;
+}
