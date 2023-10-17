d2753eac1bb8e1a8a6bd4a665a705c8392f348fa
Edvard Brekke <edvard_brekke@me.com>
2023-10-17 13:23:44 +0200

    dep

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -7,8 +7,8 @@ call sf package install --package 04t7U0000000Rf8QAE --no-prompt --installation-
 echo "Installer crm-henvendelse-base ver. 0.16"
 call sf package install --package 04t7U0000000RX4QAM --no-prompt --installation-key %3 --wait 30 --publish-wait 30
 
-echo "Installerer crm-platform-integration ver. 0.100"
-call sf package install --package 04t7U0000000RhYQAU --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+echo "Installerer crm-platform-integration ver. 0.101"
+call sf package install --package 04t7U000000om5FQAQ --no-prompt --installation-key %3 --wait 30 --publish-wait 30
 
 echo "Installerer crm-platform-access-control ver. 0.113"
 call sf package install --package 04t7U0000004e8tQAA --no-prompt --installation-key %3 --wait 30 --publish-wait 30
