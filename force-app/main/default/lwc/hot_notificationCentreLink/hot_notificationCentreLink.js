cbed6859d6372b7bef2cf795398dffaba858488a
Rasmus Steine Olsen <111959793+olsenrasmus@users.noreply.github.com>
2024-09-23 13:35:50 +0200

    Update hot_notificationCentreLink.js

--- force-app/main/default/lwc/hot_notificationCentreLink/hot_notificationCentreLink.js
+++ force-app/main/default/lwc/hot_notificationCentreLink/hot_notificationCentreLink.js
@@ -1,7 +1,5 @@
 import { LightningElement, track } from 'lwc';
-import ICON_Bell from '@salesforce/resourceUrl/Bell';
 export default class Hot_notificationCentreLink extends LightningElement {
-    bellIcon = ICON_Bell;
     @track pageLinks = {};
     connectedCallback() {
         let baseURLArray = window.location.pathname.split('/');
