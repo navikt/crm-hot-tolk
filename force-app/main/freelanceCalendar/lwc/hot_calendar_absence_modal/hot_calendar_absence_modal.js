58c0c849b83e09e6089a100dccf3604afeb632c9
Mathias <mathias.boylestad.kristensen@nav.no>
2024-10-03 15:42:48 +0200

    la til absence klasse i permsettet og noe småtteri testing

--- force-app/main/freelanceCalendar/lwc/hot_calendar_absence_modal/hot_calendar_absence_modal.js
+++ force-app/main/freelanceCalendar/lwc/hot_calendar_absence_modal/hot_calendar_absence_modal.js
@@ -1,6 +1,8 @@
 import { api } from 'lwc';
 import LightningModal from 'lightning/modal';
 import ConfimationModal from 'c/hot_calendar_absence_modal_confirmation';
+import getConflictsForTimePeriod from '@salesforce/apex/HOT_FreelanceAbsenceController.getConflictsForTimePeriod';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class Hot_Calendar_Absence_Modal extends LightningModal {
     @api content;
@@ -23,14 +25,18 @@ export default class Hot_Calendar_Absence_Modal extends LightningModal {
         // Fetch input field values
         const absenceStartDateTime = this.refs.absenceStartDateTimeInput.value;
         const absenceEndDateTime = this.refs.absenceEndDateTimeInput.value;
-
+        getConflictsForTimePeriod({
+            startTimeInMilliseconds: new Date(absenceStartDateTime).getTime(),
+            endTimeInMilliseconds: new Date(absenceEndDateTime).getTime()
+        }).then((result) => {
+            console.log(result);
+        });
         // Do something with the values
         console.log('Selected Radio Value: ', absenceType);
-        console.log('Start date/time Value: ', absenceStartDateTime);
+        console.log('Start date/time Value: ', new Date(absenceStartDateTime));
         console.log('End date/time Value: ', absenceEndDateTime);
         const isConfirmation = await ConfimationModal.open();
         if (isConfirmation) {
-            console.log('confirmation');
             this.close('resultat string');
         } else if (!isConfirmation) {
             console.log('not confirmation');
