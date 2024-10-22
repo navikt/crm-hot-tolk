992df4fe39ddfbb00eddf7cf724284ad67b19e9f
André Marthinsen <andre.reva.marthinsen@nav.no>
2024-10-22 09:04:06 +0200

    legge til mulighet for å sette manuelt klokkeslett i formatLocalDatetime

--- force-app/main/freelanceCalendar/lwc/hot_calendar_absence_modal/hot_calendar_absence_modal.js
+++ force-app/main/freelanceCalendar/lwc/hot_calendar_absence_modal/hot_calendar_absence_modal.js
@@ -192,12 +192,12 @@ export default class Hot_Calendar_Absence_Modal extends LightningModal {
     }
 
     // Hjelper metode for å få dato i riktig tidsone for dateTime input felt
-    formatLocalDateTime(date) {
+    formatLocalDateTime(date, hoursOverride, minutesOverride) {
         const year = date.getFullYear();
         const month = ('0' + (date.getMonth() + 1)).slice(-2); // Måneder er 0 indeksiert
         const day = ('0' + date.getDate()).slice(-2);
-        const hours = ('0' + date.getHours()).slice(-2);
-        const minutes = ('0' + date.getMinutes()).slice(-2);
+        const hours = hoursOverride !== undefined ? hoursOverride : ('0' + date.getHours()).slice(-2);
+        const minutes = minutesOverride !== undefined ? minutesOverride : ('0' + date.getMinutes()).slice(-2);
 
         return `${year}-${month}-${day}T${hours}:${minutes}`;
     }
