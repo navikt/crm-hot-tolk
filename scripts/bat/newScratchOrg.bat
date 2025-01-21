939c105d3c6cc519034cc092469d5e3c2ea2d394
Edvard Brekke <edvard_brekke@me.com>
2025-01-21 14:15:58 +0100

    Expands invoiceref field to 300 chars

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -1,8 +1,8 @@
 echo "Oppretter scratch org"
 call sf org create scratch --definition-file config\project-scratch-def.json --alias %1 --duration-days %2 --set-default --json --wait 30
 
-echo "Installerer crm-platform-base ver. 0.240"
-call sf package install --package 04tKB000000Y5xtYAC --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+echo "Installerer crm-platform-base ver. 0.252"
+call sf package install --package 04tKB000000Y7SbYAK --no-prompt --installation-key %3 --wait 30 --publish-wait 30
 
 echo "Installerer crm-shared-flowComponents ver. 0.4"
 call sf package install --package 04t7U0000008qz4QAA --no-prompt --installation-key %3 --wait 30 --publish-wait 30
@@ -13,8 +13,8 @@ call sf package install --package 04tKB000000Y77jYAC --no-prompt --installation-
 echo "Installerer crm-platform-integration ver. 0.133"
 call sf package install --package 04tKB000000Y1GEYA0 --no-prompt --installation-key %3 --wait 30 --publish-wait 30
 
-echo "Installerer crm-platform-access-control ver. 0.113"
-call sf package install --package 04t7U0000004e8tQAA --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+echo "Installerer crm-platform-access-control ver. 0.149"
+call sf package install --package 04tKB000000Y7q4YAC --no-prompt --installation-key %3 --wait 30 --publish-wait 30
 
 echo "Installerer crm-community-base ver. 0.119"
 call sf package install --package 04tKB000000Y0CZYA0 --no-prompt --installation-key %3 --wait 30 --publish-wait 30
