efb478665b36e24a03a9756351c7bdb391a4d5d9
Rasmus Steine Olsen <rasmus.steine.olsen@nav.no>
2025-06-10 11:48:03 +0200

    Freelance navigasjon

---
+++ force-app/main/default/lwc/hot_freelanceWrapper/hot_freelanceWrapper.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import icons from '@salesforce/resourceUrl/ikoner';
+
+export default class Hot_freelanceWrapper extends LightningElement {
+    calenderIcon = icons + '/Calender/Calender.svg';
+}
