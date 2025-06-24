dc7cbd9728d820c08b976ba9234ae3eaf29dd447
Rasmus Steine Olsen <rasmus.steine.olsen@nav.no>
2025-06-24 06:49:38 +0200

    Update hot_freelanceWrapper.js

--- force-app/main/facelift/lwc/hot_freelanceWrapper/hot_freelanceWrapper.js
+++ force-app/main/facelift/lwc/hot_freelanceWrapper/hot_freelanceWrapper.js
@@ -4,12 +4,14 @@ import icons from '@salesforce/resourceUrl/ikoner';
 export default class Hot_freelanceWrapper extends LightningElement {
     calenderIcon = icons + '/Calender/Calender.svg';
     closeIcon = icons + '/Close/Close.svg';
-    warningicon = icons + '/Warning/WarningRed.svg';
+    warningIcon = icons + '/Warning/WarningRed.svg';
+
     isUserInNorwegianTimeZone = true;
     showCalender = false;
-    static STATE_KEY = 'calendarWrapState';
 
     pageLinks = {};
+
+    static STATE_KEY = 'calendarWrapState';
 
     toggleCalender() {
         this.showCalender = !this.showCalender;
@@ -33,10 +35,15 @@ export default class Hot_freelanceWrapper extends LightningElement {
         };
     }
 
+    checkTimeZone() {
+        const osloTime = new Date().toLocaleString('no-NB', { timeZone: 'Europe/Oslo' });
+        const userTime = new Date().toLocaleString('no-NB');
+        this.isUserInNorwegianTimeZone = osloTime === userTime;
+    }
+
     connectedCallback() {
         this.checkTimeZone();
         this.setPageLinks();
-        /* Kalender */
         const state = sessionStorage.getItem(Hot_freelanceWrapper.STATE_KEY);
         if (state != null) {
             const parsedState = JSON.parse(state);
@@ -49,10 +56,5 @@ export default class Hot_freelanceWrapper extends LightningElement {
             showCalender: this.showCalender
         };
         sessionStorage.setItem(Hot_freelanceWrapper.STATE_KEY, JSON.stringify(state));
-    }
-    checkTimeZone() {
-        const osloTime = new Date().toLocaleString('no-NB', { timeZone: 'Europe/Oslo' });
-        const userTime = new Date().toLocaleString('no-NB');
-        this.isUserInNorwegianTimeZone = osloTime === userTime;
     }
 }
