b3b6740b0cfa7daf76bf358f67b6944d543d4ec0
Rasmus Steine Olsen <rasmus.steine.olsen@nav.no>
2025-06-24 07:59:09 +0200

    Legger til lenker for navigering

--- force-app/main/facelift/lwc/hot_userHome/hot_userHome.js
+++ force-app/main/facelift/lwc/hot_userHome/hot_userHome.js
@@ -1,3 +1,22 @@
 import { LightningElement } from 'lwc';
 
-export default class Hot_userHome extends LightningElement {}
+export default class Hot_userHome extends LightningElement {
+    pageLinks = {};
+
+    setPageLinks() {
+        let baseURLArray = window.location.pathname.split('/');
+        baseURLArray.pop();
+        let baseURL = baseURLArray.join('/');
+        this.pageLinks = {
+            newRequest: baseURL + '/ny-bestilling',
+            myRequests: baseURL + '/mine-bestillinger',
+            myRequestsOther: baseURL + '/mine-bestillinger-andre',
+            myPage: baseURL + '/min-side',
+            myThreads: baseURL + '/mine-samtaler'
+        };
+    }
+
+    connectedCallback() {
+        this.setPageLinks();
+    }
+}
