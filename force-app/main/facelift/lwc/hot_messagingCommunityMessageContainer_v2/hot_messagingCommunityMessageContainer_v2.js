352ecf52ff1a48c3c5da1ed0610bad015db6483e
SFBenjaminle <benjamin.le@nav.no>
2025-09-17 11:30:39 +0200

    moving old lwc message container to v2

---
+++ force-app/main/facelift/lwc/hot_messagingCommunityMessageContainer_v2/hot_messagingCommunityMessageContainer_v2.js
@@ -0,0 +1,19 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Hot_messagingCommunityMessageContainer_v2 extends LightningElement {
+    @api message;
+    @api userContactId;
+
+    userId = uId;
+
+    get isoutbound() {
+        return (
+            (this.userId && this.userId == this.message.CRM_From_User__c) ||
+            (this.userContactId && this.userContactId == this.message.CRM_From_Contact__c)
+        );
+    }
+
+    get isevent() {
+        return this.message.CRM_Type__c == 'Event';
+    }
+}
---
+++ force-app/main/facelift/lwc/hot_messagingCommunityMessageInbound_v2/hot_messagingCommunityMessageInbound_v2.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Hot_messagingCommunityMessageInbound_v2 extends LightningElement {
+    @api message;
+}
