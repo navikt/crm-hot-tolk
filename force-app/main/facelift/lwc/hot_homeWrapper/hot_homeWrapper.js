a5c926a07f6ea71f864cac575f5025164f823a48
Rasmus Steine Olsen <rasmus.steine.olsen@nav.no>
2025-10-02 09:19:43 +0200

    Bruker then kjede for å hele tiden få oppdaterte data når side lastes. Må fjerne duplikater av metode når alle bytter over til nye tolkebestilling. Cache true fjerns

--- force-app/main/facelift/lwc/hot_homeWrapper/hot_homeWrapper.js
+++ force-app/main/facelift/lwc/hot_homeWrapper/hot_homeWrapper.js
@@ -1,97 +1,68 @@
-import { LightningElement, wire } from 'lwc';
-import { refreshApex } from '@salesforce/apex';
+import { LightningElement } from 'lwc';
 import getUserInformation from '@salesforce/apex/HOT_UserInformationController.getUserInformation';
 import getAllMyThreadsForBadge from '@salesforce/apex/HOT_ThreadListController.getAllMyThreadsForBadge';
 import getContactId from '@salesforce/apex/HOT_MessageHelper.getUserContactId';
 
 export default class Hot_homeWrapper extends LightningElement {
     personDetails;
-
     isFreelance = false;
-
-    @wire(getUserInformation, {})
-    wireuser({ data }) {
-        if (data) {
-            this.personDetails = {
-                FirstName: data.FirstName,
-                ProfileName: data.Profile.Name
-            };
-            this.isFreelance = data.Profile.Name === 'NAV Samhandler';
-        }
-    }
-
-    /* FINN ULESTE MELDINGER FOR FRILANS OG BRUKER */
-
-    wiredThreadsResult;
     userContactId;
 
-    freelanceThreads = [];
     myThreads = [];
+    freelanceThreads = [];
+    unreadMyThreads = 0;
+    unreadFreelanceThreads = 0;
 
-    @wire(getAllMyThreadsForBadge, { isFreelance: '$isFreelance' })
-    wiredThreads(result) {
-        this.wiredThreadsResult = result;
-        const { data, error } = result;
-        if (data) {
-            // Setter hver liste i sin egen array
-            this.freelanceThreads = data.freelance || [];
-            this.myThreads = data.mythreads || [];
-            this.tryMapAndSortThreads();
-        } else if (error) {
-            this.freelanceThreads = [];
-            this.myThreads = [];
-        }
-    }
-    @wire(getContactId)
-    wiredContactId({ data, error }) {
-        if (data) {
-            this.userContactId = data;
-            this.tryMapAndSortThreads();
-        } else if (error) {
-            this.error = error;
-        }
-    }
+    error;
 
-    tryMapAndSortThreads() {
-        if (
-            this.userContactId &&
-            ((Array.isArray(this.freelanceThreads) && this.freelanceThreads.length > 0) ||
-                (Array.isArray(this.myThreads) && this.myThreads.length > 0))
-        ) {
-            this.mapAndSortThreads();
-        }
+    connectedCallback() {
+        this.loadData();
     }
-    unreadMyThreads;
-    unreadFreelanceThreads;
-
-    renderedCallback() {
-        refreshApex(this.wiredThreadsResult);
+    loadData() {
+        getUserInformation()
+            .then((userInfo) => {
+                this.personDetails = {
+                    FirstName: userInfo.FirstName,
+                    ProfileName: userInfo.Profile.Name
+                };
+                this.isFreelance = userInfo.Profile.Name === 'NAV Samhandler';
+                return getContactId();
+            })
+            .then((contactId) => {
+                this.userContactId = contactId;
+                return getAllMyThreadsForBadge({ isFreelance: this.isFreelance });
+            })
+            .then((threadsData) => {
+                this.freelanceThreads = threadsData.freelance || [];
+                this.myThreads = threadsData.mythreads || [];
+                this.mapAndSortThreads();
+            })
+            .catch((error) => {
+                this.error = error;
+                console.error('Feil:', error);
+                this.myThreads = [];
+                this.freelanceThreads = [];
+            });
     }
 
     mapAndSortThreads() {
+        if (!this.userContactId) return;
+
         const uid = this.userContactId;
 
-        // Mapper myThreads og teller uleste
         let unreadMyThreadsCount = 0;
-        this.sortedMyThreads = (this.myThreads ?? []).map((t) => {
+        this.myThreads = (this.myThreads ?? []).map((t) => {
             const isRead = (t.HOT_Thread_read_by__c || '').includes(uid);
             if (!isRead) unreadMyThreadsCount++;
-            return {
-                ...t,
-                isRead
-            };
+            return { ...t, isRead };
         });
         this.unreadMyThreads = unreadMyThreadsCount;
 
-        // Mapper freelanceThreads og teller uleste
         let unreadFreelanceThreadsCount = 0;
-        this.sortedFreelanceThreads = (this.freelanceThreads ?? []).map((t) => {
+        this.freelanceThreads = (this.freelanceThreads ?? []).map((t) => {
             const isRead = (t.HOT_Thread_read_by__c || '').includes(uid);
             if (!isRead) unreadFreelanceThreadsCount++;
-            return {
-                ...t,
-                isRead
-            };
+            return { ...t, isRead };
         });
         this.unreadFreelanceThreads = unreadFreelanceThreadsCount;
 
