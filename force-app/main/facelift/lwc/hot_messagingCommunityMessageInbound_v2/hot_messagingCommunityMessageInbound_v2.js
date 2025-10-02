b1df332ea756ccc68e8c1225e54dcc5cebf810cd
Rasmus Steine Olsen <rasmus.steine.olsen@nav.no>
2025-10-02 11:18:41 +0200

    Aria article annonserer samtale fra

--- force-app/main/facelift/lwc/hot_messagingCommunityMessageInbound_v2/hot_messagingCommunityMessageInbound_v2.js
+++ force-app/main/facelift/lwc/hot_messagingCommunityMessageInbound_v2/hot_messagingCommunityMessageInbound_v2.js
@@ -2,4 +2,26 @@ import { LightningElement, api } from 'lwc';
 
 export default class Hot_messagingCommunityMessageInbound_v2 extends LightningElement {
     @api message;
+
+    get date() {
+        if (!this.message.CRM_Sent_date__c) return '';
+        let rawDate = new Date(this.message.CRM_Sent_date__c);
+        if (rawDate == 'Invalid Date' || isNaN(rawDate)) {
+            console.log('navFormattedDate: Bad date format.');
+            return '';
+        }
+        // Dato
+        let formatter = new Intl.DateTimeFormat('no', { day: 'numeric', month: 'long', year: 'numeric' });
+        let date = formatter.format(rawDate);
+        // Tid
+        formatter = new Intl.DateTimeFormat('no', { hour: 'numeric', minute: 'numeric' });
+        let timeParts = formatter.formatToParts(rawDate);
+        let hour = timeParts.find((e) => e.type === 'hour').value;
+        let minute = timeParts.find((e) => e.type === 'minute').value;
+        return date + ', kl. ' + hour + '.' + minute;
+    }
+
+    get ariaLabelText() {
+        return 'Melding fra ' + this.message.HOT_User_Role__c + ', sendt ' + this.date;
+    }
 }
