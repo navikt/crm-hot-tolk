7416c478626c69e2f59369e09770ed447109e8c8
Rasmus Steine Olsen <rasmus.steine.olsen@nav.no>
2026-02-17 07:35:59 +0100

    Update hot_tindPersonHighlightPanelTop.js

--- force-app/main/personHighlightPanel/lwc/hot_tindPersonHighlightPanelTop/hot_tindPersonHighlightPanelTop.js
+++ force-app/main/personHighlightPanel/lwc/hot_tindPersonHighlightPanelTop/hot_tindPersonHighlightPanelTop.js
@@ -66,7 +66,7 @@ export default class hot_personHighlightPanelTop extends LightningElement {
             this.personDetails?.legalStatus,
             this.navUnitName
         ]
-            .filter((x) => x != null)
+            .filter(Boolean)
             .join(' / ');
     }
 
