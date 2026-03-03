89727e39d82a1b40be86b8b5d8fa367730900527
Eugenijus <eugenijus.margalikas@nav.no>
2026-03-03 14:35:24 +0100

    add mobileThreadList component

---
+++ force-app/main/dialogue/internal-components/lwc/hot_mobileThreadListCard/hot_mobileThreadListCard.js
@@ -0,0 +1,25 @@
+import { LightningElement, api } from 'lwc';
+
+export default class hot_mobileThreadListCard extends LightningElement {
+    @api thread;
+
+    handleButtonClick(event) {
+        const threadId = event.currentTarget.value;
+        this.dispatchEvent(new CustomEvent('threadbuttonclick', { detail: threadId }));
+    }
+    get fullAriaLabel() {
+        return (
+            this.thread.statusText +
+            '. Samtale ' +
+            this.thread.threadTypeName +
+            '. Tema: ' +
+            this.thread.HOT_Subject__c +
+            '. ' +
+            'Siste melding sendt: ' +
+            this.thread.lastMessageSentFormatted +
+            '. Oppdragsstart: ' +
+            this.thread.appointmentStartFormatted +
+            '.'
+        );
+    }
+}
