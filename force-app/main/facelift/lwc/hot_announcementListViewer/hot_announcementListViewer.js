0953fbde7212eacf0dde5b37636bec37993c99ae
Rasmus Steine Olsen <rasmus.steine.olsen@nav.no>
2026-05-15 08:09:33 +0200

    Nytt objekt som skal vise om det er ulest nyhet fra tolketjenesten

--- force-app/main/facelift/lwc/hot_announcementListViewer/hot_announcementListViewer.js
+++ force-app/main/facelift/lwc/hot_announcementListViewer/hot_announcementListViewer.js
@@ -1,23 +1,65 @@
 import { LightningElement, api } from 'lwc';
+
 import icons from '@salesforce/resourceUrl/ikoner';
 import ICON_Newspaper from '@salesforce/resourceUrl/Newspaper';
 
+import getUnreadAnnouncementIds from '@salesforce/apex/HOT_AnnouncementController.getUnreadAnnouncementIds';
+import markAnnouncementsAsRead from '@salesforce/apex/HOT_AnnouncementController.markAnnouncementsAsRead';
+
 export default class Hot_announcementListViewer extends LightningElement {
     @api announcements;
+
     exitCrossIcon = icons + '/Close/Close.svg';
     newspaperIcon = ICON_Newspaper;
+
     showAnnouncements = false;
     isLoading = false;
 
     announcementsToShow = 100;
     visibleAnnouncements = [];
 
-    handleShowAnnouncements() {
+    unreadAnnouncementIds = [];
+
+    connectedCallback() {
+        this.loadUnreadAnnouncements();
+    }
+
+    async loadUnreadAnnouncements() {
+        try {
+            this.unreadAnnouncementIds = await getUnreadAnnouncementIds();
+        } catch (error) {
+            console.error('Feil ved henting av uleste announcements', error);
+        }
+    }
+
+    get unreadCount() {
+        return this.unreadAnnouncementIds.length;
+    }
+
+    async handleShowAnnouncements() {
         this.updateVisibleAnnouncements();
 
         const dialog = this.template.querySelector('.modal-announcements');
+
         dialog.showModal();
         dialog.focus();
+
+        await this.setNewsRead();
+    }
+
+    async setNewsRead() {
+        if (!this.unreadAnnouncementIds.length) {
+            return;
+        }
+
+        try {
+            await markAnnouncementsAsRead();
+
+            // Fjern badge umiddelbart i UI
+            this.unreadAnnouncementIds = [];
+        } catch (error) {
+            console.error('Feil ved markering av announcements som lest', error);
+        }
     }
 
     handleShowMore() {
@@ -26,6 +68,11 @@ export default class Hot_announcementListViewer extends LightningElement {
     }
 
     updateVisibleAnnouncements() {
+        if (!this.announcements) {
+            this.visibleAnnouncements = [];
+            return;
+        }
+
         this.visibleAnnouncements = this.announcements.slice(0, this.announcementsToShow);
     }
 
@@ -35,8 +82,12 @@ export default class Hot_announcementListViewer extends LightningElement {
 
     closeModal() {
         const dialog = this.template.querySelector('.modal-announcements');
-        dialog.close();
+
+        if (dialog) {
+            dialog.close();
+        }
     }
+
     get isEmpty() {
         return !this.announcements || this.announcements.length === 0;
     }
