78118bfa8e22edcd710e4836a5f3c8bb1810de79
SFBenjaminle <benjamin.le@nav.no>
2026-05-19 10:24:15 +0200

    updated community base scratch script

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -28,8 +28,8 @@ call sf package install --package 04tQC000000xwmMYAQ --no-prompt --installation-
 echo "Installer crm-platform-access-control ver. 0.177"
 call sf package install --package 04tQC000001AGR7YAO --no-prompt --installation-key %3 --wait 30 --publish-wait 30
 
-echo "Installer crm-community-base ver. 0.133"
-call sf package install --package 04tQC0000012ZXNYA2 --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+echo "Installer crm-community-base ver. 0.143"
+call sf package install --package 04tQC000001IX9pYAG --no-prompt --installation-key %3 --wait 30 --publish-wait 30
 
 echo "Installer crm-platform-reporting ver. 0.41"
 call sf package install --package 04tKB000000YAWDYA4 --no-prompt --installation-key %3 --wait 30 --publish-wait 30
