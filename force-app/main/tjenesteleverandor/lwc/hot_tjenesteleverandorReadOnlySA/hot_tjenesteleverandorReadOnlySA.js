faa58999ab7900910fa5355eba8d1c88d6e29e2a
SFBenjaminle <benjamin.le@nav.no>
2026-07-06 09:43:45 +0200

    Added correct translation for some fields WIP

--- force-app/main/tjenesteleverandor/lwc/hot_tjenesteleverandorReadOnlySA/hot_tjenesteleverandorReadOnlySA.js
+++ force-app/main/tjenesteleverandor/lwc/hot_tjenesteleverandorReadOnlySA/hot_tjenesteleverandorReadOnlySA.js
@@ -1,5 +1,40 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import LANG from '@salesforce/i18n/lang';
+import ACCOUNT_FIELD from '@salesforce/schema/ServiceAppointment.AccountId';
+import CONTACT_FIELD from '@salesforce/schema/ServiceAppointment.ContactId';
+import WORK_TYPE_FIELD from '@salesforce/schema/ServiceAppointment.WorkTypeId';
 
-export default class Hot_tjenesteleverandorReadOnlySA extends LightningElement {
+const FIELDS = [ACCOUNT_FIELD, CONTACT_FIELD, WORK_TYPE_FIELD];
+
+export default class HOT_TjenesteleverandorSAReadOnly extends LightningElement {
     @api recordId;
+    lang = LANG;
+
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    record;
+
+    get accountId() {
+        return getFieldValue(this.record.data, ACCOUNT_FIELD);
+    }
+
+    get accountLabel() {
+        return this.lang && this.lang.toLowerCase().startsWith('no') ? 'Konto' : 'Account';
+    }
+
+    get contactId() {
+        return getFieldValue(this.record.data, CONTACT_FIELD);
+    }
+
+    get contactLabel() {
+        return this.lang && this.lang.toLowerCase().startsWith('no') ? 'Kontakt' : 'Contact';
+    }
+
+    get workTypeId() {
+        return getFieldValue(this.record.data, WORK_TYPE_FIELD);
+    }
+
+    get workTypeLabel() {
+        return this.lang && this.lang.toLowerCase().startsWith('no') ? 'Tolkemetode' : 'Work Type';
+    }
 }
