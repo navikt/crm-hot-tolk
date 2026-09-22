98bf771b7763a5aed4c3b49f8d4f9799e7f078de
SFBenjaminle <benjamin.le@nav.no>
2026-09-22 12:19:06 +0200

    updated some buttons to samtale med tjenesteleverandør

--- force-app/main/facelift/lwc/hot_messagingCommunityThreadViewer_v2/hot_messagingCommunityThreadViewer_v2.js
+++ force-app/main/facelift/lwc/hot_messagingCommunityThreadViewer_v2/hot_messagingCommunityThreadViewer_v2.js
@@ -487,6 +487,11 @@ export default class Hot_messagingCommunityThreadViewer_v2 extends NavigationMix
                 this.helptextContent =
                     'Her kan du sende en melding som er relevant for oppdraget.  Det du skriver her kan dine medtolker se.  Meldingen vil bli slettet etter ett år.';
                 return 'Samtale med medtolker';
+            case 'HOT_TJENESTELEVERANDOR-FORMIDLER':
+            case 'HOT_TJENESTELEVERANDOR-TOLK':
+                this.helptextContent =
+                    'Her kan du sende en melding som er relevant for oppdraget. Det du skriver her kan ansatte ved Nav tolketjeneste se. Meldingen vil bli slettet etter ett år.';
+                return 'Samtale med tjenesteleverandør';
             default:
                 return threadTypeValue;
         }
